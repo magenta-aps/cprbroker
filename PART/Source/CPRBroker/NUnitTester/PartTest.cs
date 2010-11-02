01fbf9c9d5857e7c4ea4f48e4fefae353b3cf602
Beemen <Beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2010-11-02 17:30:14 +0000

    Initial unit tests for Part operations

---
+++ PART/Source/CPRBroker/NUnitTester/PartTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace NUnitTester
+{
+    [TestFixture]
+    public class PartTest : BaseTest
+    {
+        #region App registrations
+        [Test]
+        public void T030_ListAppRegistrations()
+        {
+            var apps = TestRunner.AdminService.ListAppRegistrations();
+            Assert.IsNotNull(apps);
+            var targetApp = (from app in apps where app.Token == TestData.AppToken select app).SingleOrDefault();
+            Assert.IsNotNull(targetApp);
+            Assert.Greater(targetApp.RegistrationDate, DateTime.Today);
+        }
+
+        [Test]
+        public void T100_GetAndSetDataProviderList()
+        {
+            var dataProviders = TestRunner.AdminService.GetDataProviderList();
+            Assert.IsNotNull(dataProviders);
+
+            bool result = TestRunner.AdminService.SetDataProviderList(dataProviders);
+            Assert.IsTrue(result);
+        }
+        #endregion
+
+        #region Part person methods
+
+        private void ValidatePerson(Guid uuid, Part.PersonRegistration person)
+        {
+            Assert.IsNotNull(person, "Person not found : {0}", uuid);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(PartTestData), PartTestData.PersonUUIDsFieldName)]
+        public void T200_Read(Guid uuid)
+        {
+            var person = TestRunner.PartService.Read(uuid);
+            ValidatePerson(uuid, person);
+        }
+
+        [Test]
+        [TestCase(PartTestData.PersonUUIDs)]
+        public void T210_List(Guid[] personUuids)
+        {
+            var persons = TestRunner.PartService.List(personUuids);
+            Assert.IsNotNull(persons, "Persons array is null");
+            Assert.AreEqual(personUuids.Length, personUuids.Length, "Incorrect length of returned array");
+            for (int i = 0; i < personUuids.Length; i++)
+            {
+                ValidatePerson(personUuids[i], persons[i]);
+            }
+        }
+        #endregion
+
+        #region Legacy methods
+        [Test]
+        [TestCaseSource(typeof(TestData), TestData.CorrectMethodNamesFieldName)]
+        [TestCaseSource(typeof(TestData), TestData.IncorrectMethodNamesFieldName)]
+        public void T410_IsImplementing(string serviceName)
+        {
+            bool imp = TestRunner.AdminService.IsImplementing(serviceName, TestData.serviceVersion);
+            Assert.AreEqual(Array.IndexOf<string>(TestData.correctMethodNames, serviceName) != -1, imp);
+            Assert.AreNotEqual(Array.IndexOf<string>(TestData.incorrectMethodNames, serviceName) != -1, imp);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TestData), TestData.LogTextFieldName)]
+        public void T600_Log(string text)
+        {
+            bool ret = TestRunner.AdminService.Log(text);
+            Assert.IsTrue(ret);
+        }
+        #endregion
+
+
+    }
+}
--- PART/Source/CPRBroker/NUnitTester/TestRunner.cs
+++ PART/Source/CPRBroker/NUnitTester/TestRunner.cs
@@ -14,26 +14,47 @@ namespace NUnitTester
         public static CPRAdministrationWS.CPRAdministrationWS AdminService;
         public static CPRPersonWS.CPRPersonWS PersonService;
         public static Access.Access AccessService;
+        public static Part.Part PartService;
 
         public static void Initialize()
         {
             AdminService = new NUnitTester.CPRAdministrationWS.CPRAdministrationWS();
-            AdminService.ApplicationHeaderValue = new NUnitTester.CPRAdministrationWS.ApplicationHeader();
-            AdminService.ApplicationHeaderValue.ApplicationToken = TestData.BaseAppToken;
-            AdminService.ApplicationHeaderValue.UserToken = TestData.userToken;
+            AdminService.ApplicationHeaderValue = new NUnitTester.CPRAdministrationWS.ApplicationHeader()
+            {
+                ApplicationToken = TestData.BaseAppToken,
+                UserToken = TestData.userToken
+            };
             ReplaceServiceUrl(AdminService);
             Console.WriteLine(AdminService.Url);
 
+
             PersonService = new NUnitTester.CPRPersonWS.CPRPersonWS();
-            PersonService.ApplicationHeaderValue = new NUnitTester.CPRPersonWS.ApplicationHeader();
-            PersonService.ApplicationHeaderValue.ApplicationToken = TestData.BaseAppToken;
-            PersonService.ApplicationHeaderValue.UserToken = TestData.userToken;
+            PersonService.ApplicationHeaderValue = new NUnitTester.CPRPersonWS.ApplicationHeader()
+            {
+                ApplicationToken = TestData.BaseAppToken,
+                UserToken = TestData.userToken
+            };
             ReplaceServiceUrl(PersonService);
             Console.WriteLine(PersonService.Url);
 
+            
+            PartService = new NUnitTester.Part.Part();
+            PartService.ApplicationHeaderValue = new NUnitTester.Part.ApplicationHeader()
+            {
+                ApplicationToken = TestData.BaseAppToken,
+                UserToken = TestData.userToken
+            };
+            PartService.EffectDateHeaderValue = new NUnitTester.Part.EffectDateHeader() { EffectDate = null };
+            PartService.RegistrationDateHeaderValue = new NUnitTester.Part.RegistrationDateHeader() { RegistrationDate = null};
+            ReplaceServiceUrl(PartService);
+            Console.WriteLine(PartService.Url);
+
+            
             AccessService = new NUnitTester.Access.Access();
             ReplaceServiceUrl(AccessService);
             Console.WriteLine(AccessService.Url);
+            
+            
         }
 
         private static void ReplaceServiceUrl(System.Web.Services.Protocols.SoapHttpClientProtocol service)
