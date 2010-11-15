3982a8ef10ebf6da7565cc397f40e7c5c13ffcd1
Beemen <Beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2010-11-15 13:19:03 +0000

    English class representation for AS78207

--- PART/Source/CPRBroker/KMD/KmdDataProvider.AS78207.cs
+++ PART/Source/CPRBroker/KMD/KmdDataProvider.AS78207.cs
@@ -93,5 +93,528 @@ namespace CPRBroker.Providers.KMD
                 return ret;
             }
         }
+
+        public class EnglishAS78207Response
+        {
+
+            private AS78207Response InnerResponse;
+
+            public EnglishAS78207Response(AS78207Response innerResponse)
+            {
+                InnerResponse = innerResponse;
+            }
+
+            public string ReturnCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.RETURKODE;
+                }
+            }
+
+            public string ReturnText
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.RETURTEXT;
+                }
+            }
+
+            public string MunicipalityCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EKOM;
+                }
+            }
+
+            public string EINRKMD
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EINRKMD;
+                }
+            }
+
+            public string PNR
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EPNR;
+                }
+            }
+
+            public string BirthDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DFOEDS;
+                }
+            }
+
+            public string StatusKmd
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CSTATUK;
+                }
+            }
+
+            public string StatusCpr
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CSTATUC;
+                }
+            }
+
+            public string StatusDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DSTATUS;
+                }
+            }
+
+            public string PersonalMarker
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CPNRMRK;
+                }
+            }
+
+            public string TransCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CTRANS;
+                }
+            }
+
+            public string RegistrationAuthorityCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EFOEDREG;
+                }
+            }
+
+            public string RegistrationAuthorityName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.AFOEDREG;
+                }
+            }
+
+            public string Title
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ASTIL;
+                }
+            }
+
+            public string Title34
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ASTIL34;
+                }
+            }
+
+            public string PostCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CSTIL;
+                }
+            }
+
+            public string AddressingName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ANAVN;
+                }
+            }
+
+            public string AddressingName34
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ANAVN34;
+                }
+            }
+
+            public string NameMarker
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CNVMRK;
+                }
+            }
+
+            public string NameDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DNAVN;
+                }
+            }
+
+            public string NameAuthorityCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ENVNMYN;
+                }
+            }
+
+            public string NationalityCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ESTAT;
+                }
+            }
+
+            public string AddressDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DADR;
+                }
+            }
+
+            public string AddressProtection
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CADRBSK;
+                }
+            }
+
+            public string StreetCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CVEJ;
+                }
+            }
+
+            public string HouseNumber
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.AHUSNR;
+                }
+            }
+
+            public string HouseLetter
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ABOGSTV;
+                }
+            }
+
+            public string Floor
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.AETAGE;
+                }
+            }
+
+            public string DoorNumber
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ASIDOER;
+                }
+            }
+
+            public string BuildingNumber
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EBYG;
+                }
+            }
+
+            public string RelocationDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DTILFL;
+                }
+            }
+
+            public string EmigrationAddressType
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CFRAADR;
+                }
+            }
+
+            public string EmigrationPlace
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EFRASTD;
+                }
+            }
+
+            public string EmigrationAddress
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.AFRAADR;
+                }
+            }
+
+            public string ChurchRelationship
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CKIRKE;
+                }
+            }
+
+            public string DisempowermentCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CUMYND;
+                }
+            }
+
+            public string MunicipalRelationship1
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CKOMF1;
+                }
+            }
+
+            public string MunicipalRelationship2
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CKOMF2;
+                }
+            }
+
+            public string MaritallStatusCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CCIVS;
+                }
+            }
+
+            public string MaritalStatusDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DCIVS;
+                }
+            }
+
+            public string MaritalStatusAuthorityCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ECIVMYN;
+                }
+            }
+
+            public string MaritalStatusAuthorityName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ACIVMYN;
+                }
+            }
+
+            public string SpousePNR
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EPNRAEGT;
+                }
+            }
+
+            public string MotherPNR
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EPNRMOR;
+                }
+            }
+
+            public string MotherVerificationCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CVERMOR;
+                }
+            }
+
+            public string FatherPNR
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EPNRFAR;
+                }
+            }
+
+            public string FatherVerificationCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CVERFAR;
+                }
+            }
+
+            public string NumberOfChildren
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.FBOERN;
+                }
+            }
+
+            public string[] ChildrenPNRs
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EPNRBRN;
+                }
+            }
+
+            public string[] ChildrenVerificationCodes
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.CVERBRN;
+                }
+            }
+
+            public string LastName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.AEFTER;
+                }
+            }
+
+            public string FirstName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.AFORNVN;
+                }
+            }
+
+            public string CareOfName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ACONVN;
+                }
+            }
+
+            public string CareOfName34
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ACONVN34;
+                }
+            }
+
+            public string PaternityDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DFADSKB;
+                }
+            }
+
+            public string PaternityAuthorityCode
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EFADMYN;
+                }
+            }
+
+            public string SearchName
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ASOEGE;
+                }
+            }
+
+            public string SearchName34
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ASOEGE34;
+                }
+            }
+
+            public string ImmigrationDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DINDV;
+                }
+            }
+
+            public string SupplementaryAddress
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.ASUPPL;
+                }
+            }
+
+            public string AbroadDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DUDLAND;
+                }
+            }
+
+            public string DisempowermentDate
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.DUMYND;
+                }
+            }
+
+            public string DisempowermentAuthority
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EUMYND;
+                }
+            }
+
+            public string KmdCurrentPNR
+            {
+                get
+                {
+                    return this.InnerResponse.OutputRecord.EPNRGAEL;
+                }
+            }
+        }
     }
 }
