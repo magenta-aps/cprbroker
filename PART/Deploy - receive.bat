cf8fff245b472d93d607fd0b1df4adc881fa954d
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2010-12-15 14:27:43 +0000

    Deployment script

---
+++ PART/Deploy - receive.bat
@@ -0,0 +1,12 @@
+REM Extract database
+"c:\Program Files (x86)\7-Zip\7z.exe" e Deploy\PARTDB.7z -oDeploy\ -y
+
+REM Restore database
+sqlcmd -S localhost -d master -Q "RESTORE DATABASE [PART_Beemen] FROM  DISK = N'C:\Users\mag-bb\Desktop\PART\Deploy\PART.bak' WITH  FILE = 1,  MOVE N'PART' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\PART_Beemen.mdf',  MOVE N'PART_log' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\PART_Beemen_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 10"
+
+
+REM extract code files
+del /S /Q Deploy\Source\*.*
+"c:\Program Files (x86)\7-Zip\7z.exe" x Deploy\Source.7z -y -r
+XCOPY Deploy\Source\*.* Source\ /E /Y /C /Q
+del /S /Q Deploy\Source\*.*
---
+++ PART/Deploy - send.bat
@@ -0,0 +1,16 @@
+REM Backup database
+sqlcmd -U sa -P Dlph10t -S 10.20.1.20 -d PART -Q "BACKUP DATABASE [PART] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL.1\MSSQL\Backup\PART.bak' WITH NOFORMAT, INIT,  NAME = N'PART-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10"
+
+md \\5.11.118.93\c$\Users\mag-bb\Desktop\PART\Deploy\
+
+REM Zip and copy DB backup to server
+copy "\\10.20.1.20\C$\Program Files\Microsoft SQL Server\MSSQL.1\MSSQL\Backup\PART.bak" Deploy\PART.bak /Y
+"C:\Program Files\7-Zip\7z.exe" a Deploy\PARTDB.7z Deploy\PART.bak
+copy Deploy\PARTDB.7z \\5.11.118.93\c$\Users\mag-bb\Desktop\PART\Deploy\PARTDB.7z /y
+
+REM Copy code files to server
+rd /S /Q Deploy\Source
+xcopy Source\*.* Deploy\Source\*.* /E /Y /C /Q /Exclude:Deploy\excl.txt
+del Deploy\Source.7z /q
+"C:\Program Files\7-Zip\7z.exe" a Deploy\Source.7z Deploy\Source\
+copy Deploy\Source.7z \\5.11.118.93\c$\Users\mag-bb\Desktop\PART\Deploy\Source.7z /Y 
