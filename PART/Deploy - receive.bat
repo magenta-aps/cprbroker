0a3c893c1ff969d806cbd856bfc82007839fd7c7
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2010-12-26 14:48:23 +0000

    Deployment of 2 databases script

--- PART/Deploy - receive.bat
+++ PART/Deploy - receive.bat
@@ -1,12 +1,5 @@
-REM Extract database
-"c:\Program Files (x86)\7-Zip\7z.exe" e Deploy\PARTDB.bz2 -tbzip2 -oDeploy\ -y
-
-REM Drop connections
-sqlcmd -S localhost -d master -Q "DECLARE @SQL varchar(max); SET @SQL = ''; SELECT @SQL = @SQL + 'Kill ' + Convert(varchar, SPId) + ';' FROM MASTER.sys.SysProcesses WHERE DBId = DB_ID('PART_Beemen'); exec (@sql)"
-
-REM Restore database
-sqlcmd -S localhost -d master -Q "RESTORE DATABASE [PART_Beemen] FROM  DISK = N'C:\Users\mag-bb\Desktop\PART\Deploy\PART.bak' WITH  FILE = 1,  MOVE N'PART' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\PART_Beemen.mdf',  MOVE N'PART_log' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\PART_Beemen_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 10"
-
+CALL ReceiveDatabase PART
+CALL ReceiveDatabase PartEventBroker
 
 REM extract code files
 del /S /Q Deploy\Source\*.*
---
+++ PART/ReceiveDatabase.bat
@@ -0,0 +1,11 @@
+set dbName =%1
+
+REM Extract database
+"c:\Program Files (x86)\7-Zip\7z.exe" e Deploy\%dbName %.bz2 -tbzip2 -oDeploy\ -y
+
+REM Drop connections
+sqlcmd -S localhost -d master -Q "DECLARE @SQL varchar(max); SET @SQL = ''; SELECT @SQL = @SQL + 'Kill ' + Convert(varchar, SPId) + ';' FROM MASTER.sys.SysProcesses WHERE DBId = DB_ID('%dbName %_Beemen'); exec (@sql)"
+
+REM Restore database
+sqlcmd -S localhost -d master -Q "RESTORE DATABASE [%dbName %_Beemen] FROM  DISK = N'C:\Users\mag-bb\Desktop\PART\Deploy\%dbName %' WITH  FILE = 1,  MOVE N'%dbName %' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\%dbName %_Beemen.mdf',  MOVE N'%dbName %_log' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\%dbName %_Beemen_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 10"
+
