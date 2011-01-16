99586da72d6a40f7d779e5436b5532820f2520c7
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-01-16 12:34:43 +0000

    - Data change events are deleted after dequeing - Code put in critical section

--- PART/Source/CPRBroker/Engine/Events/DataChangeEventManager.cs
+++ PART/Source/CPRBroker/Engine/Events/DataChangeEventManager.cs
@@ -7,19 +7,29 @@ namespace CprBroker.Engine.Events
 {
     public class DataChangeEventManager : IDataChangeEventManager
     {
+        public static readonly object QueueLock = new object();
+
         public Schemas.Part.Events.DataChangeEventInfo[] DequeueEvents(int maxCount)
         {
-            using (var dataContext = new DAL.Events.DataChangeEventDataContext())
+            lock (QueueLock)
             {
-                var events = from ev in dataContext.DataChangeEvents
-                             orderby ev.ReceivedDate
-                             select new Schemas.Part.Events.DataChangeEventInfo()
-                             {
-                                 EventId = ev.DataChangeEventId,
-                                 PersonUuid = ev.PersonUuid,
-                                 ReceivedDate = ev.ReceivedDate
-                             };
-                return events.Take(maxCount).ToArray();
+                using (var dataContext = new DAL.Events.DataChangeEventDataContext())
+                {
+                    var events = dataContext.DataChangeEvents.Take(maxCount).ToArray();
+
+                    var ret = Array.ConvertAll<DAL.Events.DataChangeEvent, CprBroker.Schemas.Part.Events.DataChangeEventInfo>(
+                        events,
+                        ev => new Schemas.Part.Events.DataChangeEventInfo()
+                        {
+                            EventId = ev.DataChangeEventId,
+                            PersonUuid = ev.PersonUuid,
+                            ReceivedDate = ev.ReceivedDate
+                        }
+                     );
+                    dataContext.DataChangeEvents.DeleteAllOnSubmit(events);
+                    dataContext.SubmitChanges();
+                    return ret;
+                }
             }
         }
 
