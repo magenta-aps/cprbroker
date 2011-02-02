6b62250f1028cac900f89403b0bda0c4ba5a83e4
niels <niels@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-02-02 15:00:13 +0000

    standar retur codes

--- PART/Source/CPRBroker/PersonSchemas/StandardReturType.cs
+++ PART/Source/CPRBroker/PersonSchemas/StandardReturType.cs
@@ -5,13 +5,30 @@ using System.Text;
 
 namespace CprBroker.Schemas.Part
 {
+
+
+    public enum HttpErrorCode
+    {
+        OK = 200,
+        UNSPECIFIED = 500, /* Server */
+        DATASOURCE_UNAVAILABLE = 503, /* Server unavailable*/
+        BAD_CLIENT_REQUEST = 400, /* Client */
+        NOT_FOUND = 404, /*  */
+        NOT_IMPLEMENTED = 501
+    }
+    
     public partial class StandardReturType
     {
-        public static StandardReturType Create(string code, string text)
+        public static StandardReturType Create(HttpErrorCode code)
+        {
+            return Create(code, code.ToString());
+
+        }
+        public static StandardReturType Create(HttpErrorCode code, string text)
         {
             return new StandardReturType()
             {
-                StatuskodeKode = code,
+                StatuskodeKode = code.ToString(),
                 FejlbeskedTekst = text
             };
         }
