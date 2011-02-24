3e022f5c8e27e94bb75eb16e4c6a4f72a304c85c
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-02-24 09:39:52 +0000

    Removed comments from deployment script

--- PART/ReceiveDatabase.bat
+++ PART/ReceiveDatabase.bat
@@ -8,7 +8,6 @@ REM Drop connections
 sqlcmd -S IDBU02 -d master -Q "DECLARE @SQL varchar(max); SET @SQL = ''; SELECT @SQL = @SQL + 'Kill ' + Convert(varchar, SPId) + ';' FROM MASTER.sys.SysProcesses WHERE DBId = DB_ID('%dbName %_Beemen'); exec (@sql)"
 
 REM Restore database
-REM sqlcmd -S IDBU02 -d master -Q "RESTORE DATABASE [%dbName %_Beemen] FROM  DISK = N'C:\Users\mag-bb\Desktop\PART\Deploy\%dbName %' WITH  FILE = 1,  MOVE N'%dbName %' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\%dbName %_Beemen.mdf',  MOVE N'%dbName %_log' TO N'C:\NTVOL1\DB\SQL2K5\MSSQL.1\MSSQL\Data\%dbName %_Beemen_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 10"
 
 sqlcmd -S IDBU02 -d %dbName %_Beemen -i Deploy\%dbName %
 
--- PART/SendDatabase.bat
+++ PART/SendDatabase.bat
@@ -1,9 +1,5 @@
 set dbName =%1
 
-REM sqlcmd -U sa -P Dlph10t -S 10.20.1.20 -d %dbName % -Q "BACKUP DATABASE [%dbName %] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL.1\MSSQL\Backup\%dbName %.bak' WITH NOFORMAT, INIT,  NAME = N'%dbName %-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10"
-
-REM copy "\\10.20.1.20\C$\Program Files\Microsoft SQL Server\MSSQL.1\MSSQL\Backup\%dbName %.bak" Deploy\%dbName %.bak /Y
-
 del Deploy\%dbName %.sql
 
 "C:\Program Files\Microsoft SQL Server\90\Tools\Publishing\SqlPubWiz" script -d %dbName % -S 10.20.1.20 -U sa -P Dlph10t Deploy\%dbName %.sql
@@ -12,4 +8,3 @@ del Deploy\%dbName %.bz2 /F
 
 "C:\Program Files\7-Zip\7z.exe" a -tbzip2 Deploy\%dbName %.bz2 Deploy\%dbName %.sql
 
-REM copy Deploy\%dbName %.bz2 \\5.32.134.68\c$\Users\mag-bb\Desktop\PART\Deploy\%dbName %.bz2 /y
