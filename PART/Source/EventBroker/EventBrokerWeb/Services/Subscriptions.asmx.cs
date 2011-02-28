9d86e16e158d24919a83525e8ade879301b0633a
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-02-28 08:09:36 +0000

    Reinclusion of asmx services

---
+++ PART/Source/EventBroker/EventBrokerWeb/Services/Subscriptions.asmx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using CprBroker.Schemas;
+using CprBroker.Schemas.Part;
+using CprBroker.EventBroker.Subscriptions;
+
+namespace CprBroker.EventBroker.Web.Services
+{
+    /// <summary>
+    /// Contains web methods related to administration of the system
+    /// </summary>
+    [WebService(Namespace = CprBroker.Schemas.Part.ServiceNames.Namespace, Name = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Service, Description = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Service)]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    public class Subscriptions_ : GKApp.WS.wsBaseV2
+    {
+        public ApplicationHeader applicationHeader;
+        private const string ApplicationHeaderName = "applicationHeader";
+
+        public Subscriptions_()
+        {
+            BaseInit();
+        }
+
+        [SoapHeader(ApplicationHeaderName)]
+        [WebMethod(MessageName = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Methods.Subscribe, Description = CprBroker.Schemas.Part.ServiceDescription.Subscriptions.Subscribe)]
+        public BasicOutputType<ChangeSubscriptionType> Subscribe(ChannelBaseType NotificationChannel, Guid[] personUuids)
+        {
+            return Manager.Subscriptions.Subscribe(applicationHeader.UserToken, applicationHeader.ApplicationToken, NotificationChannel, personUuids);
+        }
+
+        [SoapHeader(ApplicationHeaderName)]
+        [WebMethod(MessageName = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Methods.Unsubscribe, Description = CprBroker.Schemas.Part.ServiceDescription.Subscriptions.Unsubscribe)]
+        public BasicOutputType<bool> Unsubscribe(Guid SubscriptionId)
+        {
+            return Manager.Subscriptions.Unsubscribe(applicationHeader.UserToken, applicationHeader.ApplicationToken, SubscriptionId);
+        }
+
+        [SoapHeader(ApplicationHeaderName)]
+        [WebMethod(MessageName = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Methods.SubscribeOnBirthdate, Description = CprBroker.Schemas.Part.ServiceDescription.Subscriptions.SubscribeOnBirthdate)]
+        public BasicOutputType<BirthdateSubscriptionType> SubscribeOnBirthdate(ChannelBaseType NotificationChannel, Nullable<int> Years, int PriorDays, Guid[] PersonCivilRegistrationIdentifiers)
+        {
+            return Manager.Subscriptions.SubscribeOnBirthdate(applicationHeader.UserToken, applicationHeader.ApplicationToken, NotificationChannel, Years, PriorDays, PersonCivilRegistrationIdentifiers);
+        }
+
+        [SoapHeader(ApplicationHeaderName)]
+        [WebMethod(MessageName = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Methods.RemoveBirthDateSubscription, Description = CprBroker.Schemas.Part.ServiceDescription.Subscriptions.RemoveBirthDateSubscriptions)]
+        public BasicOutputType<bool> RemoveBirthDateSubscription(Guid SubscriptionId)
+        {
+            return Manager.Subscriptions.RemoveBirthDateSubscription(applicationHeader.UserToken, applicationHeader.ApplicationToken, SubscriptionId);
+        }
+
+        [SoapHeader(ApplicationHeaderName)]
+        [WebMethod(MessageName = CprBroker.Schemas.Part.ServiceNames.Subscriptions.Methods.GetActiveSubscriptionsList, Description = CprBroker.Schemas.Part.ServiceDescription.Subscriptions.GetActiveSubscriptionsList)]
+        public BasicOutputType<SubscriptionType[]> GetActiveSubscriptionsList()
+        {
+            return Manager.Subscriptions.GetActiveSubscriptionsList(applicationHeader.UserToken, applicationHeader.ApplicationToken);
+        }
+
+    }
+}
