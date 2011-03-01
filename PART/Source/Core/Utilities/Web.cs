34ae30bf130b85b419dbd1d385ba8f993c96d433
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-03-01 14:36:56 +0000

    Added Web to Utilities

---
+++ PART/Source/Core/Utilities/Web.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+namespace CprBroker.Utilities
+{
+    public static class Web
+    {
+        public static T ObjectFromViewState<T>(StateBag bag, string key)
+        {
+            return ObjectFromViewState<T>(bag, key, default(T));
+        }
+
+        public static T ObjectFromViewState<T>(StateBag bag, string key, T defaultValue)
+        {
+            object ret = bag[key];
+            if (ret == null || !(ret is T))
+            {
+                return defaultValue;
+            }
+            else
+            {
+                return (T)ret;
+            }
+        }
+    }
+}
