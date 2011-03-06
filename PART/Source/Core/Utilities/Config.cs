3b83ad5ed57a9730798502aab3046b359cc5c77b
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-03-06 19:36:53 +0000

    Commit fix

---
+++ PART/Source/Core/Utilities/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Web;
+
+namespace CprBroker.Utilities
+{
+    public static class Config
+    {
+        public static Configuration GetConfigFile()
+        {
+            if (HttpContext.Current != null)
+            {
+                return System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~");
+            }
+            else
+            {
+                return ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            }
+        }
+    }
+}
