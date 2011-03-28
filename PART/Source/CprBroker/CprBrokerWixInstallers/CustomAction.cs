cdca26a001103f1aca6e5246c8e01825ba760b7b
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-03-28 11:48:47 +0000

    Empty custom actions added to Wix installer

---
+++ PART/Source/CprBroker/CprBrokerWixInstallers/CustomAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace CprBrokerWixInstallers
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CreateDatabase(Session session)
+        {
+            session.Log("Begin Create database");
+            System.Diagnostics.Debugger.Break();            
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult DeleteDatabase(Session session)
+        {
+            return ActionResult.Success;
+        }
+    }
+}
