c409a4a2f89ca35e73b981aacae22dcdcaff7cb0
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-04-13 14:49:30 +0000

    Wix installer can now create the database

--- PART/Source/CprBroker/CprBrokerInstallers/Properties/Resources.Designer.cs
+++ PART/Source/CprBroker/CprBrokerInstallers/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace CprBroker.Installers.CprBrokerInstallers.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace CprBroker.Installers.CprBrokerInstallers.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CprBroker.Installers.CprBrokerInstallers.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace CprBroker.Installers.CprBrokerInstallers.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,12 +61,13 @@ namespace CprBroker.Installers.CprBrokerInstallers.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to A	Absolute
-        ///B	Calculated
-        ///C	No Coordinates
+        ///   Looks up a localized string similar to Code;Name
+        ///A;Absolute
+        ///B;Calculated
+        ///C;No Coordinates
         ///.
         /// </summary>
-        internal static string AddressCoordinateQualityType {
+        public static string AddressCoordinateQualityType {
             get {
                 return ResourceManager.GetString("AddressCoordinateQualityType", resourceCulture);
             }
@@ -74,104 +75,111 @@ namespace CprBroker.Installers.CprBrokerInstallers.Properties {
         
         /// <summary>
         ///   Looks up a localized string similar to ApplicationId;Name;Token;RegistrationDate;IsApproved;ApprovedDate
-        ///{3E9890FF-0038-42A4-987A-99B63E8BC865};Base Application;07059250-E448-4040-B695-9C03F9E59E38;2009-06-25 18:20:57.043000000;True;
-        ///.
+        ///{3E9890FF-0038-42A4-987A-99B63E8BC865};Base Application;07059250-E448-4040-B695-9C03F9E59E38;2009-06-25;True;
+        ///{C98F9BE7-2DDE-404a-BAB5-5A7B1BBC3063};Event Broker;FCD568A0-8F18-4b6f-8691-C09239F158F3;2011-01-01;True;
+        ///{4A78A5C8-B39B-41B9-9707-5782DAA56E2A};CPR Business Application Demo;5f8b7af5-422e-46bb-9273-5e244dc37505;2011-01-01;True;.
         /// </summary>
-        internal static string Application {
+        public static string Application {
             get {
                 return ResourceManager.GetString("Application", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0,Ugift
-        ///1,Gift
-        ///2,RegistreretPartner
-        ///3,Separeret
-        ///4,Skilt
-        ///5,OphaevetPartnerskab
-        ///6,Enke
-        ///7,Laengstlevende.
+        ///   Looks up a localized string similar to CivilStatusCodeTypeId;Name
+        ///0;Ugift
+        ///1;Gift
+        ///2;RegistreretPartner
+        ///3;Separeret
+        ///4;Skilt
+        ///5;OphaevetPartnerskab
+        ///6;Enke
+        ///7;Laengstlevende.
         /// </summary>
-        internal static string CivilStatusCodeType {
+        public static string CivilStatusCodeType {
             get {
                 return ResourceManager.GetString("CivilStatusCodeType", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0	Email
-        ///1	Telephone
-        ///2	Other
+        ///   Looks up a localized string similar to ContactChannelTypeId;Name
+        ///0;Email
+        ///1;Telephone
+        ///2;Other
         ///.
         /// </summary>
-        internal static string ContactChannelType {
+        public static string ContactChannelType {
             get {
                 return ResourceManager.GetString("ContactChannelType", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0	iso3166alpha2
-        ///1	iso3166alpha3
-        ///2	unnumeric3
-        ///3	imk
+        ///   Looks up a localized string similar to Id;Name
+        ///0;iso3166alpha2
+        ///1;iso3166alpha3
+        ///2;unnumeric3
+        ///3;imk
         ///.
         /// </summary>
-        internal static string CountrySchemeType {
+        public static string CountrySchemeType {
             get {
                 return ResourceManager.GetString("CountrySchemeType", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to /****** Object:  ForeignKey [FK_AddressPoint_DanishAddress]    Script Date: 02/12/2011 19:13:15 ******/
+        ///   Looks up a localized string similar to /****** Object:  ForeignKey [FK_AddressPoint_DanishAddress]    Script Date: 02/27/2011 22:16:41 ******/
         ///IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N&apos;[dbo].[FK_AddressPoint_DanishAddress]&apos;) AND parent_object_id = OBJECT_ID(N&apos;[dbo].[AddressPoint]&apos;))
         ///ALTER TABLE [dbo].[AddressPoint] DROP CONSTRAINT [FK_AddressPoint_DanishAddress]
         ///GO
-        ////****** Object:  ForeignKey [FK_AddressPointStatus_AddressPoint]    Script Date: 02/12/2011 19:13:15 ******/
-        ///IF  EXISTS (SELECT * FROM sys.foreign_ [rest of string was truncated]&quot;;.
+        ////****** Object:  ForeignKey [FK_AddressPointStatus_AddressCoordinateQualityType]    Script Date: 02/27/2011 22:16:41 ******/
+        ///IF  EXISTS (SELECT * F [rest of string was truncated]&quot;;.
         /// </summary>
-        internal static string CreatePartDatabaseObjects {
+        public static string CreatePartDatabaseObjects {
             get {
                 return ResourceManager.GetString("CreatePartDatabaseObjects", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0	Male
-        ///1	Female
-        ///2	Unspecified
+        ///   Looks up a localized string similar to GenderId;GenderName
+        ///0;Male
+        ///1;Female
+        ///2;Unspecified
         ///.
         /// </summary>
-        internal static string Gender {
+        public static string Gender {
             get {
                 return ResourceManager.GetString("Gender", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0	Created
-        ///1	Imported
-        ///2	Deactivated
-        ///3	Deleted
-        ///4	Updated
+        ///   Looks up a localized string similar to LifecycleStatusId;LifecycleStatusName
+        ///0;Created
+        ///1;Imported
+        ///2;Deactivated
+        ///3;Deleted
+        ///4;Updated
         ///.
         /// </summary>
-        internal static string LifecycleStatus {
+        public static string LifecycleStatus {
             get {
                 return ResourceManager.GetString("LifecycleStatus", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0	Prenatal
-        ///1	Foedt
-        ///2	Forsvundet
-        ///3	Doed
+        ///   Looks up a localized string similar to LifeStatusCodeTypeId;Name
+        ///0;Prenatal
+        ///1;Foedt
+        ///2;Forsvundet
+        ///3;Doed
         ///.
         /// </summary>
-        internal static string LifeStatusCodeType {
+        public static string LifeStatusCodeType {
             get {
                 return ResourceManager.GetString("LifeStatusCodeType", resourceCulture);
             }
@@ -191,28 +199,29 @@ namespace CprBroker.Installers.CprBrokerInstallers.Properties {
         ///4096;Transfer
         ///.
         /// </summary>
-        internal static string LogType {
+        public static string LogType {
             get {
                 return ResourceManager.GetString("LogType", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0	Mother
-        ///1	Father
-        ///2	Children
-        ///3	Spouse
-        ///4	Registered partner
-        ///5	Residence collection
-        ///6	Custody
-        ///7	Parenting adult children
-        ///8	Guardian of person
-        ///9	Guardianship owner
-        ///10	Replacement for
-        ///11	Replaced by
+        ///   Looks up a localized string similar to RelationshipTypeId;ForwardName
+        ///0;Mother
+        ///1;Father
+        ///2;Children
+        ///3;Spouse
+        ///4;Registered partner
+        ///5;Residence collection
+        ///6;Custody
+        ///7;Parenting adult children
+        ///8;Guardian of person
+        ///9;Guardianship owner
+        ///10;Replacement for
+        ///11;Replaced by
         ///.
         /// </summary>
-        internal static string RelationshipType {
+        public static string RelationshipType {
             get {
                 return ResourceManager.GetString("RelationshipType", resourceCulture);
             }
--- PART/Source/CprBroker/CprBrokerWixInstallers/CustomAction.cs
+++ PART/Source/CprBroker/CprBrokerWixInstallers/CustomAction.cs
@@ -11,59 +11,6 @@ namespace CprBrokerWixInstallers
     public class CustomActions
     {
         [CustomAction]
-        public static ActionResult TestConnectionString(Session session)
-        {
-            try
-            {
-                DatabaseSetupInfo dbInfo = GetDatabaseSetupInfo(session);
-                string message = "";
-                if (dbInfo.Validate(ref message))
-                {
-                    session["DB_VALID"] = "True";
-                }
-                else
-                {
-                    session["DB_VALID"] = message;
-                }
-                return ActionResult.Success;
-            }
-            catch (Exception ex)
-            {
-                System.Windows.Forms.MessageBox.Show(ex.Message);
-                return ActionResult.Failure;
-            }
-        }
-
-        private static DatabaseSetupInfo GetDatabaseSetupInfo(Session session)
-        {
-            DatabaseSetupInfo ret = new DatabaseSetupInfo();
-            ret.ServerName = session["DB_SERVERNAME"];
-            ret.DatabaseName = session["DB_DATABASENAME"];
-
-            ret.AdminAuthenticationInfo = new DatabaseSetupInfo.AuthenticationInfo();
-            ret.AdminAuthenticationInfo.IntegratedSecurity = session["DB_ADMININTEGRATEDSECURITY"] == "SSPI";
-            if (!ret.AdminAuthenticationInfo.IntegratedSecurity)
-            {
-                ret.AdminAuthenticationInfo.UserName = session["DB_ADMINUSERNAME"];
-                ret.AdminAuthenticationInfo.Password = session["DB_ADMINPASSWORD"];
-            }
-
-            ret.ApplicationAuthenticationSameAsAdmin = !string.IsNullOrEmpty(session["DB_APPSAMEASADMIN"]);
-            if (!ret.ApplicationAuthenticationSameAsAdmin)
-            {
-                ret.ApplicationAuthenticationInfo = new DatabaseSetupInfo.AuthenticationInfo();
-                ret.ApplicationAuthenticationInfo.IntegratedSecurity = session["DB_APPINTEGRATEDSECURITY"] == "SSPI";
-                if (!ret.ApplicationAuthenticationInfo.IntegratedSecurity)
-                {
-                    ret.ApplicationAuthenticationInfo.UserName = session["DB_APPUSERNAME"];
-                    ret.ApplicationAuthenticationInfo.Password = session["DB_APPPASSWORD"];
-                }
-            }
-
-            return ret;
-        }
-
-        [CustomAction]
         public static ActionResult PopulateWebSites(Session session)
         {
             DirectoryEntry w3svc = new DirectoryEntry(WebInstallationInfo.ServerRoot);
