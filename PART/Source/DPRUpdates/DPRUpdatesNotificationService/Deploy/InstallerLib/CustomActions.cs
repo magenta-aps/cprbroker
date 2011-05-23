982077326769807f9cd5d24435279642d9a27794
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-05-23 11:00:08 +0000

    Draft version of DPR updates installer

---
+++ PART/Source/DPRUpdates/DPRUpdatesNotificationService/Deploy/InstallerLib/CustomActions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using CprBroker.Installers;
+
+namespace InstallerLib
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult TestDatabaseConnection(Session session)
+        {
+            return DatabaseCustomAction.TestConnectionString(session);
+
+            return ActionResult.Success;
+        }
+    }
+}
