c01c0e88b0ae00b56dcb251ce1400f8957bed9b1
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-10-23 19:11:32 +0000

    Initial unit tests for E&M relationships

---
+++ PART/Source/CprBroker/E_M/Child.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Schemas.Part;
+using NUnit.Framework;
+
+namespace CprBroker.Providers.E_M
+{
+    public partial class Child
+    {
+        public static PersonFlerRelationType ToPersonFlerRelationType(Child child, Func<string, Guid> cpr2uuidFunc)
+        {
+            if (child != null)
+            {
+                return PersonFlerRelationType.Create(cpr2uuidFunc(Converters.ToCprNumber(child.PNR)), null, null);
+            }
+            return null;
+        }
+
+        [TestFixture]
+        public class Tests
+        {
+            private Dictionary<string, Guid> uuids = new Dictionary<string, Guid>();
+            [TearDown]
+            public void ClearUUIDList()
+            {
+                uuids.Clear();
+            }
+            Guid ToUuid(string cprNumber)
+            {
+                var ret = Guid.NewGuid();
+                uuids[cprNumber] = ret;
+                return ret;
+            }
+            decimal?[] TestCprNumbers
+            {
+                get { return UnitTests.RandomCprNumbers(10); }
+            }
+
+            [Test]
+            [TestCaseSource("TestCprNumbers")]
+            public void TestToPersonFlerRelationType(decimal? cprNumber)
+            {
+                var result = Child.ToPersonFlerRelationType(new Child() { PNR = cprNumber }, ToUuid);
+                var stringCprNumber = Converters.ToCprNumber(cprNumber);                
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.ReferenceID);
+                Assert.IsTrue(uuids.ContainsKey(stringCprNumber));
+                Assert.AreEqual(uuids[stringCprNumber].ToString(), result.ReferenceID.Item);
+            }
+
+        }
+    }
+}
--- PART/Source/CprBroker/E_M/UnitTests.cs
+++ PART/Source/CprBroker/E_M/UnitTests.cs
@@ -44,5 +44,20 @@ namespace CprBroker.Providers.E_M
                 Assert.IsNotNull(result);
             }
         }
+
+        public static decimal?[] RandomCprNumbers(int count)
+        {
+            var cprNumbers = new List<decimal?>();
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                var day = random.Next(1, 29).ToString("00");
+                var month = random.Next(1, 13).ToString("00");
+                var year = random.Next(1, 100).ToString("00");
+                var part1 = random.Next(1000, 9999).ToString();
+                cprNumbers.Add(decimal.Parse(day + month + year + part1));
+            }
+            return cprNumbers.ToArray();
+        }
     }
 }
