72748c00af74bac19d62f5431d2480a44e1b512d
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-10-25 16:01:20 +0000

    E&M assembly info modification

--- PART/Source/CprBroker/E_M.Tests/Properties/AssemblyInfo.cs
+++ PART/Source/CprBroker/E_M.Tests/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("E_M.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit tests for E_M project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Magenta ApS")]
-[assembly: AssemblyProduct("E_M.Tests")]
+[assembly: AssemblyProduct("CPR Broker")]
 [assembly: AssemblyCopyright("Copyright © Magenta ApS 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
