42f2a1e5fe872f6681584c9dc41f39b5cb04ee44
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-10-25 10:24:32 +0000

    E&M CitizenStatesTests methods split over many single assert methods

--- PART/Source/CprBroker/E_M.Tests/CitizenStatesTests.cs
+++ PART/Source/CprBroker/E_M.Tests/CitizenStatesTests.cs
@@ -11,31 +11,86 @@ namespace CprBroker.Tests.E_M
     [TestFixture]
     class CitizenStatesTests
     {
-            [TestFixtureSetUp]
-            public void InitializeTestCitizens()
-            {
-
-            }
-            private Citizen[] TestCitizens = new Citizen[]
-            {
-                null,
-                new Citizen(){CitizenStatusCode = 1, CitizenStatusTimestamp = DateTime.Today, CitizenStatusTimestampUncertainty='T',MaritalStatus='U',MaritalStatusTimestamp=DateTime.Today, MaritalStatusTimestampUncertainty='T'}
-            };
-
-            [Test]
-            [TestCaseSource("TestCitizens")]
-            public void TestToCivilStatusType(Citizen citizen)
-            {
-                var result = Citizen.ToCivilStatusType(citizen);
-                UnitTests.ValidateNulls<Citizen, CivilStatusType>(citizen, result);
-            }
-
-            [Test]
-            [TestCaseSource("TestCitizens")]
-            public void TestToLivStatusType(Citizen citizen)
-            {
-                var result = Citizen.ToLivStatusType(citizen);
-                UnitTests.ValidateNulls<Citizen, LivStatusType>(citizen, result);         
+        public CitizenStatesTests()
+        {
+            InitializeTestCitizens();
         }
+
+        [SetUp]
+        public void InitializeTestCitizens()
+        {
+            TestCitizen = new Citizen() { CitizenStatusCode = 1, CitizenStatusTimestamp = DateTime.Today, CitizenStatusTimestampUncertainty = 'T', MaritalStatus = 'U', MaritalStatusTimestamp = DateTime.Today, MaritalStatusTimestampUncertainty = 'T' };
+            TestCitizens = new Citizen[] { TestCitizen };
+        }
+
+        private Citizen TestCitizen = null;
+        private Citizen[] TestCitizens = null;
+
+        #region ToCivilStatusType
+        [Test]
+        public void ToCivilStatusType_Null_Null()
+        {
+            var result = Citizen.ToCivilStatusType(null);
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        [TestCaseSource("TestCitizens")]
+        public void ToCivilStatusType_Valid_NotNull(Citizen citizen)
+        {
+            var result = Citizen.ToCivilStatusType(citizen);
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [TestCaseSource("TestCitizens")]
+        public void ToCivilStatusType_Valid_CorrectCode(Citizen citizen)
+        {
+            var result = Citizen.ToCivilStatusType(citizen);
+            Assert.AreEqual(Converters.ToCivilStatusKodeType(citizen.MaritalStatus), result.CivilStatusKode);
+        }
+
+        [Test]
+        [TestCaseSource("TestCitizens")]
+        public void ToCivilStatusType_Valid_TilstandVirkningNotNull(Citizen citizen)
+        {
+            var result = Citizen.ToCivilStatusType(citizen);
+            Assert.NotNull(result.TilstandVirkning);
+        }
+        #endregion
+
+        #region ToLivStatusType
+        [Test]
+        public void ToLivStatusType_Null_Null()
+        {
+            var result = Citizen.ToLivStatusType(null);
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        [TestCaseSource("TestCitizens")]
+        public void ToLivStatusType_Valid_NotNull(Citizen citizen)
+        {
+            var result = Citizen.ToLivStatusType(citizen);
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [TestCaseSource("TestCitizens")]
+        public void ToLivStatusType_Valid_CorrectCode(Citizen citizen)
+        {
+            var result = Citizen.ToLivStatusType(citizen);
+            Assert.AreEqual(Converters.ToLivStatusKodeType(citizen.CitizenStatusCode, citizen.Birthdate), result.LivStatusKode);
+        }
+
+        [Test]
+        [TestCaseSource("TestCitizens")]
+        public void ToLivStatusType_Valid_TilstandVirkningNotNull(Citizen citizen)
+        {
+            var result = Citizen.ToLivStatusType(citizen);
+            Assert.NotNull(result.TilstandVirkning);
+        }
+        #endregion
+
     }
 }
