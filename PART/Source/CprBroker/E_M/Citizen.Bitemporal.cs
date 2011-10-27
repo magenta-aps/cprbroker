6d73ad27d4e2d3d1209038f8280c8985f82ace15
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-10-27 12:30:28 +0000

    Removed unnecessary data objects from E&M data layer - DeadCitizen - ActiveCitizen - Resettlement - Renamed Citizen.ArrivalDate to RelocationDate - Modified citizen effect dates as needed

--- PART/Source/CprBroker/E_M/Citizen.Bitemporal.cs
+++ PART/Source/CprBroker/E_M/Citizen.Bitemporal.cs
@@ -15,34 +15,26 @@ namespace CprBroker.Providers.E_M
             var dates = new List<DateTime?>();
             dates.AddRange(new DateTime?[]
             {           
-                
-                    Converters.ToDateTime(citizen.Birthdate,citizen.BirthdateUncertainty),
-                    Converters.ToDateTime(citizen.CitizenStatusTimestamp,citizen.CitizenStatusTimestampUncertainty),                    
-                    citizen.CpcNationalChurchTimestamp,
-                    citizen.CprPersonTimestamp,
-                    Converters.ToDateTime(citizen.DepartureTimestamp,citizen.DepartureTimestampUncertainty),
-                    Converters.ToDateTime(citizen.DirectoryProtectionEndDate,citizen.DirectoryProtectionRemovalDateUncertainty),
-                    Converters.ToDateTime(citizen.DiretoryProtectionDate, citizen.DirectoryProtectionDateUncertainty),
-                    Converters.ToDateTime(citizen.MaritalStatusTimestamp,citizen.MaritalStatusTimestampUncertainty),
-                    Converters.ToDateTime(citizen.MaritalStatusTerminationTimestamp,citizen.MaritalStatusTerminationTimestampUncertainty),
-                    Converters.ToDateTime(citizen.MunicipalityArrivalDate,citizen.MunicipalityArrivalTimestampUncertainty),
-                    Converters.ToDateTime(citizen.NationalChurchMarkerDate, citizen.NationalChurchMarkerUncertainty),
-                    Converters.ToDateTime(citizen.NationalityChangeTimestamp,citizen.NationalityChangeTimestampUncertainty),
-                    Converters.ToDateTime(citizen.NationalityTerminationTimestamp,citizen.NationalityTerminationTimestampUncertainty),
-                    Converters.ToDateTime(citizen.OccupationDate,citizen.OccupationDateUncertainty),
-                    Converters.ToDateTime(citizen.PNRCreationDate, citizen.PnrCreationdateUncertainty),
-                    Converters.ToDateTime(citizen.PNRMarkingDate,citizen.PnrMarkingDateUncertainty)                    
+                Converters.ToDateTime(citizen.Birthdate,citizen.BirthdateUncertainty),
+                Converters.ToDateTime(citizen.CitizenStatusTimestamp,citizen.CitizenStatusTimestampUncertainty),                    
+                citizen.CpcNationalChurchTimestamp,
+                citizen.CprPersonTimestamp,
+                Converters.ToDateTime(citizen.DepartureTimestamp,citizen.DepartureTimestampUncertainty),
+                Converters.ToDateTime(citizen.DirectoryProtectionEndDate,citizen.DirectoryProtectionRemovalDateUncertainty),
+                Converters.ToDateTime(citizen.DiretoryProtectionDate, citizen.DirectoryProtectionDateUncertainty),
+                Converters.ToDateTime(citizen.MaritalStatusTimestamp,citizen.MaritalStatusTimestampUncertainty),
+                Converters.ToDateTime(citizen.MaritalStatusTerminationTimestamp,citizen.MaritalStatusTerminationTimestampUncertainty),
+                Converters.ToDateTime(citizen.MunicipalityArrivalDate,citizen.MunicipalityArrivalTimestampUncertainty),
+                Converters.ToDateTime(citizen.NationalChurchMarkerDate, citizen.NationalChurchMarkerUncertainty),
+                Converters.ToDateTime(citizen.NationalityChangeTimestamp,citizen.NationalityChangeTimestampUncertainty),
+                Converters.ToDateTime(citizen.NationalityTerminationTimestamp,citizen.NationalityTerminationTimestampUncertainty),
+                Converters.ToDateTime(citizen.OccupationDate,citizen.OccupationDateUncertainty),
+                Converters.ToDateTime(citizen.PNRCreationDate, citizen.PnrCreationdateUncertainty),
+                Converters.ToDateTime(citizen.PNRMarkingDate,citizen.PnrMarkingDateUncertainty),
+                Converters.ToDateTime(citizen.RelocationTimestamp,citizen.RelocationTimestampUncertainty),
             });
-            // TODO: Move this in the Resettlement class
-            foreach (var res in citizen.Resettlements)
-            {
-                dates.Add(Converters.ToDateTime(res.ArrivalTimestamp, res.ArrivalTimestampUncertainty));
-                dates.Add(res.CprResettlementTimestamp);
-                dates.Add(Converters.ToDateTime(res.DepartureTimestamp, res.DepartureTimestampUncertainty));
-                dates.Add(Converters.ToDateTime(res.MunicipalityArrivalDate, res.MunicipalityArrivalDateUncertainty));
-                dates.Add(Converters.ToDateTime(res.MunicipalityDepartureDate, res.MunicipalityDepartureDateUncertainty));
-            }
-            return VirkningType.Create(Converters.GetMaxDate(dates.ToArray()), null);
+            var maxDate = Converters.GetMaxDate(dates.ToArray());
+            return VirkningType.Create(maxDate, null);
 
         }
 
