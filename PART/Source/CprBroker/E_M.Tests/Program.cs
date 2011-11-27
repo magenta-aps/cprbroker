7a48a617ce6bfe05c481229d1edbcf0785a2744f
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-11-27 15:27:02 +0000

    E&M test application can now test using a web service

--- PART/Source/CprBroker/E_M.Tests/Program.cs
+++ PART/Source/CprBroker/E_M.Tests/Program.cs
@@ -9,6 +9,10 @@ namespace CprBroker.Tests.E_M
 {
     public class Program
     {
+        string ConnectionString;
+        string ServiceUrl = "http://CprBroker/Services/Part.asmx";
+        string ApplicationToken = "fb6e6e79-7505-4667-9736-d500092d844b";
+
         public static void Main(string[] args)
         {
             new Program().Run(args);
@@ -16,11 +20,11 @@ namespace CprBroker.Tests.E_M
 
         public void Run(string[] args)
         {
-            var nowDirectoryString = string.Format("Runs\\{0}\\", DateTime.Now.ToString("yyyy MM dd hh_mm"));
+            var nowDirectoryString = string.Format("Runs\\{0}\\", DateTime.Now.ToString("yyyy MM dd HH_mm"));
             if (!Directory.Exists(nowDirectoryString))
                 Directory.CreateDirectory(nowDirectoryString);
 
-            string connectionString = string.Join(" ", args);
+            ConnectionString = string.Join(" ", args);
             string logFileName = nowDirectoryString + "Log.txt";
             string succeededFileName = nowDirectoryString + "Succeeded.txt";
             string failedFileName = nowDirectoryString + "Failed.txt";
@@ -38,11 +42,22 @@ namespace CprBroker.Tests.E_M
 
             var effectDate = DateTime.Today;
 
+            bool useWebService = true;
+            Func<string, DateTime, object> getter;
+            if (useWebService)
+            {
+                getter = GetPersonFromWebService;
+            }
+            else
+            {
+                getter = GetPersonFromDatabase;
+            }
+
             using (succeededFileWriter)
             {
                 using (failedFileWriter)
                 {
-                    using (var dataContext = new E_MDataContext(connectionString))
+                    using (var dataContext = new E_MDataContext(ConnectionString))
                     {
                         var count = dataContext.Citizens.Count();
                         Console.WriteLine(string.Format("Found <{0}>citizens", count));
@@ -53,7 +68,7 @@ namespace CprBroker.Tests.E_M
                             Console.WriteLine(string.Format("Starting citizen <{0}>", pnr));
                             try
                             {
-                                var registration = citizen.ToRegistreringType1(effectDate, cpr => Guid.NewGuid());
+                                var registration = getter(pnr, effectDate);
                                 string registrationXml = CprBroker.Utilities.Strings.SerializeObject(registration);
                                 string registrationFileName = string.Format("{0}{1}.xml", outDir, pnr);
                                 File.WriteAllText(registrationFileName, registrationXml);
@@ -76,6 +91,39 @@ namespace CprBroker.Tests.E_M
                 }
             }
 
+        }
+
+        object GetPersonFromDatabase(string pnr, DateTime effectDate)
+        {
+            using (var dataContext = new E_MDataContext(ConnectionString))
+            {
+                var citizen = dataContext.Citizens.Where(cit => cit.PNR == decimal.Parse(pnr)).First();
+                var registration = citizen.ToRegistreringType1(effectDate, cpr => Guid.NewGuid());
+                return registration;
+            }
+        }
+
+        object GetPersonFromWebService(string pnr, DateTime effectDate)
+        {
+            var partService = new Part.Part();
+            partService.ApplicationHeaderValue = new CprBroker.Tests.E_M.Part.ApplicationHeader()
+            {
+                ApplicationToken = ApplicationToken,
+                UserToken = ""
+            };
+            partService.Url = ServiceUrl;
+
+            var uuid = partService.GetUuid(pnr);
+            if (uuid.StandardRetur.StatusKode != "200")
+            {
+                throw new Exception(string.Format("GetUuid failed : {0}: {1}", uuid.StandardRetur.StatusKode, uuid.StandardRetur.FejlbeskedTekst));
+            }
+            var person = partService.RefreshRead(new CprBroker.Tests.E_M.Part.LaesInputType() { UUID = uuid.UUID });
+            if (person.StandardRetur.StatusKode != "200")
+            {
+                throw new Exception(string.Format("Read failed : {0}: {1}", person.StandardRetur.StatusKode, person.StandardRetur.FejlbeskedTekst));
+            }
+            return person.LaesResultat.Item;
         }
 
     }
