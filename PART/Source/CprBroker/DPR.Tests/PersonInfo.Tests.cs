6d6321c385fce21871f63f95d9a2610a551b42b7
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2011-12-13 19:50:16 +0000

    New project: DPR.Tests - Added project - Added PersonInfoTests

---
+++ PART/Source/CprBroker/DPR.Tests/PersonInfo.Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CprBroker.Providers.DPR;
+using CprBroker.Utilities;
+using CprBroker.Utilities.ConsoleApps;
+
+namespace CprBroker.Tests.DPR
+{
+    [TestFixture]
+    class PersonInfoTests : ConsoleEnvironment
+    {
+        public PersonInfoTests(string[] args)
+            : base(args)
+        { }
+
+        public static void Main(string[] args)
+        {
+            // OtherConnectionString = "Data Source=10.20.1.20;Database=DPR;User ID=sa;Password=Dlph10t"
+            var test = new PersonInfoTests(args);
+            test.Run();
+        }
+
+        [Test]
+        [TestCaseSource("AllCprNumbers")]
+        public void GetPersonInfo_Normal_EqualsPersonInfoExpression(decimal pnr)
+        {
+            using (var dataContext = new DPRDataContext(OtherConnectionString))
+            {
+                DateTime effectDate = DateTime.Now;
+                // UUID mapping
+                var map = new Dictionary<string, Guid>();
+                Func<string, Guid> func = (string cpr) =>
+                {
+                    if (!map.ContainsKey(cpr))
+                    {
+                        map[cpr] = Guid.NewGuid();
+                    }
+                    return map[cpr];
+                };
+
+                var simplifiedPersonInfo = PersonInfo.GetPersonInfo(dataContext, pnr);
+                Assert.NotNull(simplifiedPersonInfo, "simplifiedPersonInfo");
+                var simplifiedPersonRegistration = simplifiedPersonInfo.ToRegisteringType1(effectDate, func, dataContext);
+                Assert.NotNull(simplifiedPersonRegistration, "simplifiedPersonRegistration");
+                var simplifiedXml = CprBroker.Utilities.Strings.SerializeObject(simplifiedPersonRegistration);
+                WriteObject(pnr.ToDecimalString(), simplifiedXml);
+
+                var expressionPersonInfo = PersonInfo.PersonInfoExpression.Compile()(dataContext).Where(pi => pi.PersonTotal.PNR == pnr).FirstOrDefault();
+                if (expressionPersonInfo != null)
+                {
+                    var expressionPersonRegistration = expressionPersonInfo.ToRegisteringType1(effectDate, func, dataContext);
+                    Assert.NotNull(expressionPersonRegistration, "expressionPersonRegistration");
+                    var expressionXml = CprBroker.Utilities.Strings.SerializeObject(expressionPersonRegistration);
+                    WriteObject(pnr.ToDecimalString() + "-expr", expressionXml);
+                    Assert.AreEqual(expressionXml, simplifiedXml);
+                }
+
+            }
+        }
+
+        public override void ProcessPerson(string pnr)
+        {
+            decimal decimalPnr = decimal.Parse(pnr);
+            GetPersonInfo_Normal_EqualsPersonInfoExpression(decimalPnr);
+        }
+
+        public override string[] LoadCprNumbers()
+        {
+            using (var dataContext = new DPRDataContext(OtherConnectionString))
+            {
+                return dataContext.PersonTotals.OrderBy(pt => pt.PNR).Select(pt => pt.PNR.ToString()).ToArray();
+            }
+        }
+    }
+}
