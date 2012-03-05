76dab3a35b4ae03d805a3e52b5e047f6d0c6377b
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-03-05 14:03:30 +0000

    Added custom actions to CPR Broker installer to forget previous versions

--- PART/Source/CprBroker/CprBrokerWixInstallers/CprBrokerCustomActions.cs
+++ PART/Source/CprBroker/CprBrokerWixInstallers/CprBrokerCustomActions.cs
@@ -62,6 +62,34 @@ namespace CprBrokerWixInstallers
     public class CprBrokerCustomActions
     {
         [CustomAction]
+        public static ActionResult AfterInstallInitialize_Product(Session session)
+        {
+            try
+            {
+                return ProductCustomActions.AfterInstallInitialize_Product(session);
+            }
+            catch (Exception ex)
+            {
+                session.ShowErrorMessage(ex);
+                throw ex;
+            }
+        }
+
+        [CustomAction]
+        public static ActionResult ForgetOlderVersions(Session session)
+        {
+            try
+            {
+                return ProductCustomActions.ForgetOlderVersions(session);
+            }
+            catch (Exception ex)
+            {
+                session.ShowErrorMessage(ex);
+                throw ex;
+            }
+        }
+
+        [CustomAction]
         public static ActionResult AppSearch_DB(Session session)
         {
             try
