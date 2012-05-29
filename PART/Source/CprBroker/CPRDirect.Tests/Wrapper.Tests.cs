534ce703bf63c9573604093208665df7564ce317
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-05-29 12:46:59 +0000

    Added CPR Direct Tests Added basic Wrapper class for CPR Direct

---
+++ PART/Source/CprBroker/CPRDirect.Tests/Wrapper.Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Providers.CPRDirect;
+using NUnit.Framework;
+
+namespace CprBroker.Tests.CPRDirect
+{
+    [TestFixture]
+    public class WrapperTests
+    {
+        class WrapperStub : Wrapper
+        {
+            public int _Length;
+            public override int Length
+            {
+                get { return _Length; }
+            }
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Contents_InvalidLength_ThrowsException(
+            [Random(0, 10000, 5)]int len,
+            [Values(1, 30, -12)]int diff)
+        {
+            var w = new WrapperStub() { _Length = len };
+            w.Contents = new string('s', len + diff);
+        }
+
+        [Test]
+        public void Contents_InvalidLength_ThrowsException(
+            [Random(0, 10000, 5)]int len)
+        {
+            var w = new WrapperStub() { _Length = len };
+            var val = new string('s', len);
+            w.Contents = val;
+            Assert.AreEqual(val, w.Contents);
+        }
+    }
+}
---
+++ PART/Source/CprBroker/CPRDirect/Wrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.Providers.CPRDirect
+{
+    public abstract class Wrapper
+    {
+        private string _Contents;
+        public string Contents
+        {
+            get { return _Contents; }
+            set
+            {
+                int len = string.Format("{0}", value).Length;
+                if (len != Length)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Contents",
+                        value,
+                        string.Format("Should be exactly {0} characters", Length)
+                        );
+                }
+                _Contents = value;
+            }
+        }
+
+        public abstract int Length { get; }
+
+    }
+}
