5bff5cda0d1e2eb25895f135dc995205d26490a5
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-06-10 13:10:18 +0000

    Test code refactorization

--- PART/Source/Core/Utilities.Tests/StringsTests.cs
+++ PART/Source/Core/Utilities.Tests/StringsTests.cs
@@ -50,29 +50,35 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using CprBroker.Utilities;
+using CprBroker.Schemas.Part;
 
 namespace CprBroker.Tests.Utilities
 {
-    [TestFixture]
-    class StringsTests
+    namespace StringsTests
     {
-        string[] ValidHostNames = new string[] { "hotmail.com", "personMaster", "1PersonMaster", "999pms.dk", "m.a.a", "code-valley", "c.m-ee.12kl", "ddd234", "kkk---ppp", "88-22", "f.92.15xyz", "12.code.abc" };
-        string[] InvalidHostNames = new string[] { "", "-ddd", "def-", "22", "77.76", "klm.", "..", "....." };
-
-        [Test]
-        [TestCaseSource("ValidHostNames")]
-        public void IsValidHostName_OK_ReturnsTrue(string hostName)
+        [TestFixture]
+        public class IsValidHostName
         {
-            var result = CprBroker.Utilities.Strings.IsValidHostName(hostName);
-            Assert.True(result);
+            string[] ValidHostNames = new string[] { "hotmail.com", "personMaster", "1PersonMaster", "999pms.dk", "m.a.a", "code-valley", "c.m-ee.12kl", "ddd234", "kkk---ppp", "88-22", "f.92.15xyz", "12.code.abc" };
+            string[] InvalidHostNames = new string[] { "", "-ddd", "def-", "22", "77.76", "klm.", "..", "....." };
+
+            [Test]
+            [TestCaseSource("ValidHostNames")]
+            public void IsValidHostName_OK_ReturnsTrue(string hostName)
+            {
+                var result = CprBroker.Utilities.Strings.IsValidHostName(hostName);
+                Assert.True(result);
+            }
+
+            [Test]
+            [TestCaseSource("InvalidHostNames")]
+            public void IsValidHostName_Invalid_ReturnsFalse(string hostName)
+            {
+                var result = CprBroker.Utilities.Strings.IsValidHostName(hostName);
+                Assert.False(result);
+            }
         }
 
-        [Test]
-        [TestCaseSource("InvalidHostNames")]
-        public void IsValidHostName_Invalid_ReturnsFalse(string hostName)
-        {
-            var result = CprBroker.Utilities.Strings.IsValidHostName(hostName);
-            Assert.False(result);
-        }
+       
     }
 }
