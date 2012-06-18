ed6636a11f80afaf4bdc17699cc022cd8475a4da
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-06-18 15:17:24 +0000

    Unit tests for IndividualRequest

---
+++ PART/Source/CprBroker/CPRDirect.Tests/IndividualRequestTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CprBroker.Providers.CPRDirect;
+
+namespace CprBroker.Tests.CPRDirect
+{
+    namespace IndividualRequestTests
+    {
+        [TestFixture]
+        public class IndividualRequestType_
+        {
+            [Test]
+            public void IndividualRequestType_Pnr_CorrectPnr(
+                [Values(0, 1, 12, 56, 123456789)]decimal pnr,
+                [Values(true, false)] bool subscription)
+            {
+                var req = new IndividualRequestType(subscription, pnr);
+                Assert.AreEqual(pnr, req.PNR);
+            }
+        }
+    }
+}
