3f5b6331ce57b18efd4259a327ecf54d52db9179
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-07-01 09:51:36 +0000

    Implementation of GetUuidsFromPersonMaster in BatchClient - ungeneric implementation

---
+++ PART/Source/CprBroker/BatchClient/GetUuidsFromPersonMaster.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using CprBroker.Schemas;
+using CprBroker.Schemas.Part;
+using CprBroker.Utilities.ConsoleApps;
+//using CprBroker.Providers.PersonMaster.PersonMasterService;
+namespace BatchClient
+{
+    class GetUuidsFromPersonMaster : GetUuids
+    {
+        public override string[] LoadCprNumbers()
+        {
+            var baseRet = base.LoadCprNumbers();
+            int batchSize = 500;
+            var ret = new List<string>();
+            var myRet = new List<string>(batchSize);
+            for (int i = 0; i < baseRet.Length; i++)
+            {
+                myRet.Add(baseRet[i]);
+                if (myRet.Count == batchSize || i == baseRet.Length - 1)
+                {
+                    ret.Add(string.Join(",", myRet.ToArray()));
+                    myRet.Clear();
+                }
+            }
+            return ret.ToArray();
+        }
+
+        int Succeeded = 0;
+        int Failed = 0;
+
+        string PersonMasterUrl = "";
+        string SpnName = "";
+
+        public override void ProcessPerson(string joinedPnrBatch)
+        {
+            WSHttpBinding binding = new WSHttpBinding();
+
+            var identity = new SpnEndpointIdentity(SpnName);
+            EndpointAddress endPointAddress = new EndpointAddress(new Uri(PersonMasterUrl), identity);
+            PersonMaster.BasicOpClient client = new PersonMaster.BasicOpClient(binding, endPointAddress);
+            string[] pnrs = joinedPnrBatch.Split(',');
+            string aux = "";
+            var result = client.GetObjectIDsFromCprArray("BacthClient", pnrs, ref aux);
+            Succeeded += result.Where(res => res.HasValue).Count();
+            Succeeded += result.Where(res => !res.HasValue).Count();
+            Console.WriteLine(string.Format("Batch finished: Size={0}, Succeeded So far={1}; Failed So far={2}", pnrs.Length, Succeeded, Failed));
+        }
+    }
+}
