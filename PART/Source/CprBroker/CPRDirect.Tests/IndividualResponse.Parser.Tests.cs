3b04ef66802c42c56f27b3d33ddecc6727f035a3
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-07-10 16:10:36 +0000

    Split all CPR direct test data by person in separate file per person

--- PART/Source/CprBroker/CPRDirect.Tests/IndividualResponse.Parser.Tests.cs
+++ PART/Source/CprBroker/CPRDirect.Tests/IndividualResponse.Parser.Tests.cs
@@ -12,6 +12,36 @@ namespace CprBroker.Tests.CPRDirect
         [TestFixture]
         public class ParseBatch
         {
+            [Test]
+            public void ParseAll___()
+            {
+
+                var rd = new System.IO.StringReader(Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE);
+
+                var dataLines = rd.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Where(l => l.Length >= 3);
+                var allLineWrappers = dataLines.Select(line => new LineWrapper(line));
+
+                var start = allLineWrappers.Where(w => w.IntCode == 0).First();
+                var end = allLineWrappers.Where(w => w.IntCode == 999).First();
+
+                allLineWrappers = allLineWrappers.Where(w => w.IntCode != 0 && w.IntCode != 999).ToArray();
+
+                var groupedWrapers = allLineWrappers
+                    .GroupBy(w => w.PNR);
+
+                foreach (var group in groupedWrapers)
+                {
+                    var list = new List<LineWrapper>(group.ToList());
+                    list.Insert(0, start);
+                    list.Add(end);
+
+                    var pnrLines = list.Select(w => w.Contents).ToArray();
+                    var data = string.Join("\r\n", pnrLines.ToArray());
+                    System.IO.File.WriteAllLines(
+                        string.Format(@"..\..\Resources\PNR_{0}.txt", new LineWrapper(pnrLines[2]).PNR),
+                        pnrLines);
+                }
+            }
 
             [Test]
             public void ParseBatch_ChangeExtract_80Persons()
