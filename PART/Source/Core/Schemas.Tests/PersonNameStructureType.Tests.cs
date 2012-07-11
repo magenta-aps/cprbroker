d1d7c22c0ab993568112d5ef7e0adf389a36da14
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-07-11 11:01:58 +0000

    Unit tests for PersonNameStructureType

---
+++ PART/Source/Core/Schemas.Tests/PersonNameStructureType.Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CprBroker.Schemas.Part;
+
+namespace CprBroker.Tests.Schemas
+{
+    namespace PersonNameStructureTypeTests
+    {
+        [TestFixture]
+        public class Constructor
+        {
+            [Test]
+            public void Constructor_NoComma_OK()
+            {
+                var names = new string[] { "John", "Smith" };
+                var ret = new PersonNameStructureType(names);
+                Assert.AreEqual("John", ret.PersonGivenName);
+                Assert.AreEqual("Smith", ret.PersonSurnameName);
+            }
+
+            [Test]
+            public void Constructor_Comma_OK()
+            {
+                var names = new string[] { "Smith, John" };
+                var ret = new PersonNameStructureType(names);
+                Assert.AreEqual("John", ret.PersonGivenName);
+                Assert.AreEqual("Smith", ret.PersonSurnameName);
+            }
+        }
+    }
+}
