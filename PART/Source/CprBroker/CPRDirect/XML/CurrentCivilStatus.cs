5929396a8f874d40e675d9acec3ccb210c5edd08
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-07-16 16:16:58 +0000

    Code Isolation

---
+++ PART/Source/CprBroker/CPRDirect/XML/CurrentCivilStatus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Schemas.Part;
+
+namespace CprBroker.Providers.CPRDirect
+{
+    public partial class CurrentCivilStatusType : ICivilStatus
+    {
+        string ICivilStatus.ToSpousePnr()
+        {
+            return Converters.ToPnrStringOrNull(this.SpousePNR);
+        }
+
+        DateTime? ICivilStatus.ToCivilStatusStartDate()
+        {
+            return Converters.ToDateTime(this.CivilStatusStartDate, this.CivilStatusStartDateUncertainty);
+        }
+
+        DateTime? ICivilStatus.ToCivilStatusEndDate()
+        {
+            return null;
+        }
+
+        bool ICivilStatus.IsValid()
+        {
+            return true;
+        }
+    }
+
+    public partial class HistoricalCivilStatusType : ICivilStatus
+    {
+        bool ICivilStatus.IsValid()
+        {
+            return Converters.IsValidCorrectionMarker(this.CorrectionMarker);
+        }
+
+        string ICivilStatus.ToSpousePnr()
+        {
+            return Converters.ToPnrStringOrNull(this.SpousePNR);
+        }
+
+        DateTime? ICivilStatus.ToCivilStatusStartDate()
+        {
+            return Converters.ToDateTime(this.CivilStatusStartDate, this.CivilStatusStartDateUncertainty);
+        }
+
+        DateTime? ICivilStatus.ToCivilStatusEndDate()
+        {
+            return Converters.ToDateTime(this.CivilStatusEndDate, this.CivilStatusEndDateUncertainty);
+        }
+    }
+
+    public partial class CurrentSeparationType : ISeparation
+    {
+ 
+    }
+}
