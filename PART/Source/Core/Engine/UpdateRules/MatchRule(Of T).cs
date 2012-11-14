7ccd4388b3c0987a57bcfd40db4b301eb7f8ecb8
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-11-14 16:49:43 +0000

    Split MatchRule and MatchRule<T> in 2 files

---
+++ PART/Source/Core/Engine/UpdateRules/MatchRule(Of T).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Utilities;
+using CprBroker.Schemas.Part;
+using CprBroker.Data.Part;
+
+namespace CprBroker.Engine.UpdateRules
+{
+    public abstract class MatchRule<TOio> : MatchRule
+    {
+        public override sealed bool UpdateOioFromXmlIfPossible(RegistreringType1 existingReg, RegistreringType1 newReg)
+        {
+            var existingObj = GetObject(existingReg);
+            var newObj = GetObject(newReg);
+
+            if (existingObj != null && newObj != null)
+            {
+                if (AreCandidates(existingObj, newObj))
+                {
+                    UpdateOioFromXmlType(existingObj, newObj);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override sealed void UpdateDbFromXmlType(PersonRegistration dbReg, RegistreringType1 newObj)
+        {
+            var obj = GetObject(newObj);
+            UpdateDbFromXmlType(dbReg, obj);
+        }
+
+        public abstract TOio GetObject(RegistreringType1 oio);
+        public abstract bool AreCandidates(TOio existingObj, TOio newObj);
+        public abstract void UpdateOioFromXmlType(TOio existingObj, TOio newObj);
+        public abstract void UpdateDbFromXmlType(PersonRegistration dbReg, TOio newObj);
+    }
+}
--- PART/Source/Core/Engine/UpdateRules/MatchRule.cs
+++ PART/Source/Core/Engine/UpdateRules/MatchRule.cs
@@ -59,34 +59,4 @@ namespace CprBroker.Engine.UpdateRules
             return false;
         }
     }
-
-    public abstract class MatchRule<TOio> : MatchRule
-    {
-        public override sealed bool UpdateOioFromXmlIfPossible(RegistreringType1 existingReg, RegistreringType1 newReg)
-        {
-            var existingObj = GetObject(existingReg);
-            var newObj = GetObject(newReg);
-
-            if (existingObj != null && newObj != null)
-            {
-                if (AreCandidates(existingObj, newObj))
-                {
-                    UpdateOioFromXmlType(existingObj, newObj);
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public override sealed void UpdateDbFromXmlType(PersonRegistration dbReg, RegistreringType1 newObj)
-        {
-            var obj = GetObject(newObj);
-            UpdateDbFromXmlType(dbReg, obj);
-        }
-
-        public abstract TOio GetObject(RegistreringType1 oio);
-        public abstract bool AreCandidates(TOio existingObj, TOio newObj);
-        public abstract void UpdateOioFromXmlType(TOio existingObj, TOio newObj);
-        public abstract void UpdateDbFromXmlType(PersonRegistration dbReg, TOio newObj);
-    }
 }
