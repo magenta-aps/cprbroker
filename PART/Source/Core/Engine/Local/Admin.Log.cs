7092ea3aa1155444076fe2713f33f9f7d2475924
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2012-11-28 16:45:38 +0000

    TODO comments

--- PART/Source/Core/Engine/Local/Admin.Log.cs
+++ PART/Source/Core/Engine/Local/Admin.Log.cs
@@ -88,21 +88,25 @@ namespace CprBroker.Engine.Local
         /// <param name="ex"></param>
         public static void LogException(Exception ex)
         {
+            // TODO: Use BrokerContex.WebMethodMessageName
             AddNewLog(TraceEventType.Error, ex.Source, ex.ToString(), null, null);
         }
 
         public static void LogCriticalException(Exception ex)
         {
+            // TODO: Use BrokerContex.WebMethodMessageName
             AddNewLog(TraceEventType.Critical, ex.Source, ex.ToString(), null, null);
         }
 
         public static void LogException(Exception ex, string moreText)
         {
+            // TODO: Use BrokerContex.WebMethodMessageName
             AddNewLog(TraceEventType.Error, ex.Source, string.Format("{0}{1}{2}", moreText, Environment.NewLine, ex.ToString()), null, null);
         }
 
         public static void LogException(Exception ex, Exception[] moreExceptions)
         {
+            // TODO: Use BrokerContex.WebMethodMessageName
             string[] msgs = Array.ConvertAll<Exception, string>(moreExceptions, e => e.ToString());
             AddNewLog(TraceEventType.Error, ex.Source, string.Format("{0}{1}{2}", ex.ToString(), Environment.NewLine, string.Join(Environment.NewLine, msgs)), null, null);
         }
