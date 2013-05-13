e0a12564ca23007840c315969463474aa06492d4
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-05-13 14:09:42 +0000

    Subscription classes are now generated as part of main XSD classes

--- PART/Source/Core/Schemas/OIOXSD/XSD.cs
+++ PART/Source/Core/Schemas/OIOXSD/XSD.cs
@@ -4350,4 +4350,176 @@ namespace CprBroker.Schemas.Part {
             }
         }
     }
+    
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/")]
+    [System.Xml.Serialization.XmlRootAttribute("BirthdateSubscription", Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/", IsNullable=false)]
+    public partial class BirthdateSubscriptionType : SubscriptionType {
+        
+        private System.Nullable<int> ageYearsField;
+        
+        private int priorDaysField;
+        
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(IsNullable=true)]
+        public System.Nullable<int> AgeYears {
+            get {
+                return this.ageYearsField;
+            }
+            set {
+                this.ageYearsField = value;
+            }
+        }
+        
+        /// <remarks/>
+        public int PriorDays {
+            get {
+                return this.priorDaysField;
+            }
+            set {
+                this.priorDaysField = value;
+            }
+        }
+    }
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(BirthdateSubscriptionType))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(ChangeSubscriptionType))]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/")]
+    public abstract partial class SubscriptionType {
+        
+        private string subscriptionIdField;
+        
+        private string applicationTokenField;
+        
+        private ChannelBaseType notificationChannelField;
+        
+        private bool forAllPersonsField;
+        
+        private string[] personUuidsField;
+        
+        /// <remarks/>
+        public string SubscriptionId {
+            get {
+                return this.subscriptionIdField;
+            }
+            set {
+                this.subscriptionIdField = value;
+            }
+        }
+        
+        /// <remarks/>
+        public string ApplicationToken {
+            get {
+                return this.applicationTokenField;
+            }
+            set {
+                this.applicationTokenField = value;
+            }
+        }
+        
+        /// <remarks/>
+        public ChannelBaseType NotificationChannel {
+            get {
+                return this.notificationChannelField;
+            }
+            set {
+                this.notificationChannelField = value;
+            }
+        }
+        
+        /// <remarks/>
+        public bool ForAllPersons {
+            get {
+                return this.forAllPersonsField;
+            }
+            set {
+                this.forAllPersonsField = value;
+            }
+        }
+        
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("PersonUuids")]
+        public string[] PersonUuids {
+            get {
+                return this.personUuidsField;
+            }
+            set {
+                this.personUuidsField = value;
+            }
+        }
+    }
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(FileShareChannelType))]
+    [System.Xml.Serialization.XmlIncludeAttribute(typeof(WebServiceChannelType))]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/")]
+    public abstract partial class ChannelBaseType {
+    }
+    
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/")]
+    [System.Xml.Serialization.XmlRootAttribute("ChangeSubscription", Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/", IsNullable=false)]
+    public partial class ChangeSubscriptionType : SubscriptionType {
+    }
+    
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/")]
+    [System.Xml.Serialization.XmlRootAttribute("WebServiceChannel", Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/", IsNullable=false)]
+    public partial class WebServiceChannelType : ChannelBaseType {
+        
+        private string webServiceUrlField;
+        
+        /// <remarks/>
+        public string WebServiceUrl {
+            get {
+                return this.webServiceUrlField;
+            }
+            set {
+                this.webServiceUrlField = value;
+            }
+        }
+    }
+    
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/")]
+    [System.Xml.Serialization.XmlRootAttribute("FileShareChannel", Namespace="http://rep.oio.dk/cpr.dk/xml/schemas/2009/07/01/", IsNullable=false)]
+    public partial class FileShareChannelType : ChannelBaseType {
+        
+        private string pathField;
+        
+        /// <remarks/>
+        public string Path {
+            get {
+                return this.pathField;
+            }
+            set {
+                this.pathField = value;
+            }
+        }
+    }
 }
