5ed5b0c98cca4ba900b2834713193fc59d3c5a2f
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-05-15 16:13:40 +0000

    DtaChangeEvent.PersonRegistrationId is now not null

--- PART/Source/EventBroker/EventBrokerInstallers/Resources/PatchDatabase_2_2.sql
+++ PART/Source/EventBroker/EventBrokerInstallers/Resources/PatchDatabase_2_2.sql
@@ -42,7 +42,7 @@ GO
 ---------------------------
 
 IF NOT EXISTS (SELECT * FROM sys.columns c WHERE name = 'PersonRegistrationId' and object_id=object_id('DataChangeEvent'))
-	ALTER TABLE dbo.DataChangeEvent ADD PersonRegistrationId UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID()
+	ALTER TABLE dbo.DataChangeEvent ADD PersonRegistrationId UNIQUEIDENTIFIER NOT NULL
 GO
 
 ------------------------
