dd418eba4964fe439d571e29dcda2b5ba85e70e8
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-05-28 12:01:11 +0000

    Reintergrated period methods into Part.asmx

--- PART/Source/CprBroker/Web/Services/Part.asmx.cs
+++ PART/Source/CprBroker/Web/Services/Part.asmx.cs
@@ -124,6 +124,41 @@ namespace CprBroker.Web.Services
             return Manager.Part.PutSubscription(applicationHeader.UserToken, applicationHeader.ApplicationToken, personUuids);
         }
 
+        [WebMethod(Description = "Gets a single snapshot of one person's CPR data")]
+        [SoapHeader(ApplicationHeaderName)]
+        [SoapHeader(SourceUsageOrderHeaderName, Direction = SoapHeaderDirection.In)]
+        public LaesOutputType ReadSnapshot(string UUID, DateTime virkningDato)
+        {
+            var sourceUsageOrder = SourceUsageOrderHeader.GetLocalDataProviderUsageOption(this.sourceUsageOrderHeader);
+            return Manager.Period.ReadAtTime(UUID, virkningDato, applicationHeader.ApplicationToken, applicationHeader.UserToken, sourceUsageOrder);
+        }
+
+        [WebMethod(Description = "Gets all snapshots of one person's CPR data that fall between the input effect dates")]
+        [SoapHeader(ApplicationHeaderName)]
+        [SoapHeader(SourceUsageOrderHeaderName, Direction = SoapHeaderDirection.In)]
+        public LaesOutputType ReadPeriod(string UUID, DateTime fraVirkningDato, DateTime tilVirkningDato)
+        {
+            var sourceUsageOrder = SourceUsageOrderHeader.GetLocalDataProviderUsageOption(this.sourceUsageOrderHeader);
+            return Manager.Period.ReadPeriod(UUID, fraVirkningDato, tilVirkningDato, applicationHeader.ApplicationToken, applicationHeader.UserToken, sourceUsageOrder);
+        }
+
+        [WebMethod(Description = "Gets a single snapshot of the given persons' CPR data")]
+        [SoapHeader(ApplicationHeaderName)]
+        [SoapHeader(SourceUsageOrderHeaderName, Direction = SoapHeaderDirection.In)]
+        public ListOutputType1 ListSnapshot(string[] UUIDs, DateTime virkningDato)
+        {
+            var sourceUsageOrder = SourceUsageOrderHeader.GetLocalDataProviderUsageOption(this.sourceUsageOrderHeader);
+            return Manager.Period.ListAtTime(UUIDs, virkningDato, applicationHeader.ApplicationToken, applicationHeader.UserToken, sourceUsageOrder);
+        }
+
+        [WebMethod(Description = "Gets all snapshots of the given persons' CPR data that fall between the input effect dates")]
+        [SoapHeader(ApplicationHeaderName)]
+        [SoapHeader(SourceUsageOrderHeaderName, Direction = SoapHeaderDirection.In)]
+        public ListOutputType1 ListPeriod(string[] UUIDs, DateTime fraVirkningDato, DateTime tilVirkningDato)
+        {
+            var sourceUsageOrder = SourceUsageOrderHeader.GetLocalDataProviderUsageOption(this.sourceUsageOrderHeader);
+            return Manager.Period.ListPeriod(UUIDs, fraVirkningDato, tilVirkningDato, applicationHeader.ApplicationToken, applicationHeader.UserToken, sourceUsageOrder);
+        }
     }
 
 }
--- PART/Source/CprBroker/Web/Services/Period.asmx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebService Language="C#" CodeBehind="Period.asmx.cs" Class="CprBroker.Web.Services.Period" %>
