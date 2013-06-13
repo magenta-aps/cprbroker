4ac2f526e47689bb76d154cb2fa7ef84380ae59a
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-06-13 14:47:08 +0000

    Aded parental authority to data type tags

--- PART/Source/Core/Schemas/Intervals/DataTypeTags.cs
+++ PART/Source/Core/Schemas/Intervals/DataTypeTags.cs
@@ -67,6 +67,7 @@ namespace CprBroker.Schemas.Part
         Citizenship,
         BirthRegistration,
         BasicInformation,
-        Disempowerment
+        Disempowerment,
+        ParentalAuthority
     }
 }
