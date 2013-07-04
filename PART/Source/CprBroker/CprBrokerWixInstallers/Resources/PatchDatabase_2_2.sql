1f9ba29c978112ed0f48bc7517de546427e80deb
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-07-04 13:43:15 +0000

    Adjusted CP Broker database patch to work

--- PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_2.sql
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_2.sql
@@ -34,17 +34,19 @@ GO
 ------------------
 
 IF NOT EXISTS(SELECT * FROM sys.columns c WHERE name = 'ProcessedLines' and object_id=object_id('Extract'))
-	ALTER TABLE dbo.Extrcat ADD ProcessedLines BIGINT NULL
+	ALTER TABLE dbo.Extract ADD ProcessedLines BIGINT NULL
 GO
 
 ------------------------------
 -- Table : ExtractPersonStaging
 ------------------------------
 
-CREATE NONCLUSTERED INDEX [IX_ExtractPersonStaging_ExtractId] ON [dbo].[ExtractPersonStaging] 
-(
-	[ExtractId] ASC
-)WITH (STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_ExtractPersonStaging_ExtractId' AND object_id=object_id('ExtractPersonStaging'))
+	CREATE NONCLUSTERED INDEX [IX_ExtractPersonStaging_ExtractId] ON [dbo].[ExtractPersonStaging] 
+	(
+		[ExtractId] ASC
+	)
+	WITH (STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
 
 
@@ -60,25 +62,86 @@ GO
 -- Getting rid of unused tables
 ----------------------------------------------------------
 
-drop table dbo.PersonLifeState
-drop table dbo.PersonCivilState
-drop table dbo.PersonState
-drop table dbo.PersonRelationship
-drop table dbo.UnknownCitizenData
-drop table dbo.PersonName
-drop table dbo.PersonProperties
-drop table dbo.HealthInformation
-drop table dbo.ForeignCitizenCountry
-drop table dbo.ForeignCitizenData
-drop table dbo.CprData
-drop table dbo.PersonAttributes
-drop table dbo.ForeignAddress
-drop table dbo.GreenlandicAddress
-drop table dbo.GeographicCoorditaneTuple
-drop table dbo.GeographicPointLocation
-drop table dbo.AddressPointStatus
-drop table dbo.AddressPoint
-drop table dbo.DanishAddress
-drop table dbo.DenmarkAddress
-drop table dbo.Address
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonLifeState'))
+	drop table dbo.PersonLifeState
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonCivilState'))
+	drop table dbo.PersonCivilState
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonState'))
+	drop table dbo.PersonState
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonRelationship'))
+	drop table dbo.PersonRelationship
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('UnknownCitizenData'))
+	drop table dbo.UnknownCitizenData
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonName'))
+	drop table dbo.PersonName
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonProperties'))
+	drop table dbo.PersonProperties
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('HealthInformation'))
+	drop table dbo.HealthInformation
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('ForeignCitizenCountry'))
+	drop table dbo.ForeignCitizenCountry
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('ForeignCitizenData'))
+	drop table dbo.ForeignCitizenData
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('CprData'))
+	drop table dbo.CprData
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('PersonAttributes'))
+	drop table dbo.PersonAttributes
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('ForeignAddress'))
+	drop table dbo.ForeignAddress
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('GreenlandicAddress'))
+	drop table dbo.GreenlandicAddress
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('GeographicCoorditaneTuple'))
+	drop table dbo.GeographicCoorditaneTuple
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('GeographicPointLocation'))
+	drop table dbo.GeographicPointLocation
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('AddressPointStatus'))
+	drop table dbo.AddressPointStatus
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('AddressPoint'))
+	drop table dbo.AddressPoint
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('DanishAddress'))
+	drop table dbo.DanishAddress
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('DenmarkAddress'))
+	drop table dbo.DenmarkAddress
+GO
+
+IF EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('Address'))
+	drop table dbo.Address
 GO
