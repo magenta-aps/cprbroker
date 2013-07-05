539595f6cafd1202cba9e84406d03eae45454682
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-07-05 09:01:31 +0000

    Added DB script to create table SubscriptionCriteriaMatch

--- PART/Source/EventBroker/EventBrokerInstallers/Resources/PatchDatabase_2_2.sql
+++ PART/Source/EventBroker/EventBrokerInstallers/Resources/PatchDatabase_2_2.sql
@@ -26,14 +26,16 @@ GO
 -- Table : SubscriptionCriteriaMatch
 -------------------------------------
 
-CREATE TABLE [dbo].[SubscriptionCriteriaMatch](
-	[SubscriptionCriteriaMatchId] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
-	[SubscriptionId] [uniqueidentifier] NOT NULL,
-	[DataChangeEventId] [uniqueidentifier] NOT NULL,
-	CONSTRAINT [PK_SubscriptionCriteriaMatch] PRIMARY KEY CLUSTERED ( [SubscriptionCriteriaMatchId] ASC) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY],
-	CONSTRAINT [FK_SubscriptionCriteriaMatch_DataChangeEvent] FOREIGN KEY([DataChangeEventId]) REFERENCES [dbo].[DataChangeEvent] ([DataChangeEventId]) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT [FK_SubscriptionCriteriaMatch_Subscription] FOREIGN KEY([SubscriptionId]) REFERENCES [dbo].[Subscription] ([SubscriptionId]) ON UPDATE CASCADE ON DELETE CASCADE
-) ON [PRIMARY]
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = object_id('SubscriptionCriteriaMatch'))
+
+	CREATE TABLE [dbo].[SubscriptionCriteriaMatch](
+		[SubscriptionCriteriaMatchId] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+		[SubscriptionId] [uniqueidentifier] NOT NULL,
+		[DataChangeEventId] [uniqueidentifier] NOT NULL,
+		CONSTRAINT [PK_SubscriptionCriteriaMatch] PRIMARY KEY CLUSTERED ( [SubscriptionCriteriaMatchId] ASC) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY],
+		CONSTRAINT [FK_SubscriptionCriteriaMatch_DataChangeEvent] FOREIGN KEY([DataChangeEventId]) REFERENCES [dbo].[DataChangeEvent] ([DataChangeEventId]) ON UPDATE CASCADE ON DELETE CASCADE,
+		CONSTRAINT [FK_SubscriptionCriteriaMatch_Subscription] FOREIGN KEY([SubscriptionId]) REFERENCES [dbo].[Subscription] ([SubscriptionId]) ON UPDATE CASCADE ON DELETE CASCADE
+	) ON [PRIMARY]
 
 GO
 
