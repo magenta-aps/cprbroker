a42c18e8fa4c38f3d8dcfd94e0f56cf954e6ea65
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-07-22 11:32:56 +0000

    Updated E&M Updates database creation script

--- PART/Source/Updates/E_MUpdatesNotificationService/Resources/create_EM_objects.sql
+++ PART/Source/Updates/E_MUpdatesNotificationService/Resources/create_EM_objects.sql
@@ -34,6 +34,20 @@ truncate table AA70300T_TMP2
 GO
 
 /*************************************************************************
+	Create table T_E_MUpdateStaging_Excluded
+*************************************************************************/
+
+IF NOT EXISTS (SELECT * FROM sysobjects where name='T_E_MUpdateStaging_Excluded')
+	CREATE TABLE dbo.T_E_MUpdateStaging_Excluded
+	(
+		Id int NOT NULL IDENTITY (1, 1),
+		PERSONNUMMER decimal(11, 0) NOT NULL,
+		E_MTable varchar(120) NOT NULL,
+		CreateTS datetime NOT NULL
+	)  ON [PRIMARY]
+GO
+
+/*************************************************************************
 						THE PREPARE PROCEDURE
 	This is the procedure called before a KMD synchronizations is
 	initiated.
@@ -56,6 +70,10 @@ GO
 	This is where our comparison takes place.
 *************************************************************************/
 
+IF EXISTS (SELECT * FROM sys.procedures where name = 'usp_CPR_Broker_Compare')
+	DROP PROCEDURE [dbo].[usp_CPR_Broker_Compare]
+GO
+
 CREATE PROCEDURE [dbo].[usp_CPR_Broker_Compare] AS
 	BEGIN
 		SET NOCOUNT ON
@@ -63,13 +81,15 @@ CREATE PROCEDURE [dbo].[usp_CPR_Broker_Compare] AS
 		INSERT INTO AA70300T_TMP2 SELECT * FROM AA70300T;
         EXEC xp_logevent 70000, 'usp_CPR_Broker_Compare is starting...', informational
 
+		DECLARE @T_E_MUpdateStaging_Tmp Table(PERSONNUMMER DECIMAL(11,0), E_MTable VARCHAR(120), CreateTS DateTime, Valid BIT)
+
 		IF (EXISTS (SELECT PERSONNUMMER FROM AA70000T_TMP1) AND EXISTS (SELECT PERSONNUMMER FROM AA70000T_TMP2))
             EXEC xp_logevent 70000, '... both AA70000 tables contain data...', informational
 			-- We check if the temp tables actually contains data to avoid mistakedly updates --
 			BEGIN
 				-- We look for updates in already existing records --
-				INSERT INTO T_E_MUpdateStaging (PERSONNUMMER, E_MTable, CreateTS) 
-					SELECT PERSONNUMMER, 'AA70000T', current_timestamp
+				INSERT INTO @T_E_MUpdateStaging_Tmp (PERSONNUMMER, E_MTable, CreateTS, Valid) 
+					SELECT PERSONNUMMER, 'AA70000T', current_timestamp, 0
 					FROM (
 						SELECT PERSONNUMMER, COUNT(*) AS PNR_COUNT  FROM (
 							SELECT * FROM AA70000T_TMP1 
@@ -81,8 +101,8 @@ CREATE PROCEDURE [dbo].[usp_CPR_Broker_Compare] AS
 					WHERE PNR_COUNT > 1
                 EXEC xp_logevent 70000, '... checked for differences...', informational
 				-- We look for new records --
-				INSERT INTO T_E_MUpdateStaging (PERSONNUMMER, E_MTable, CreateTS) 
-					SELECT t2.PERSONNUMMER, 'AA70000T', current_timestamp 
+				INSERT INTO @T_E_MUpdateStaging_Tmp (PERSONNUMMER, E_MTable, CreateTS, Valid) 
+					SELECT t2.PERSONNUMMER, 'AA70000T', current_timestamp, 0 
 					FROM AA70000T_TMP2 t2 LEFT OUTER JOIN AA70000T_TMP1 t1
 					ON t2.PERSONNUMMER = t1.PERSONNUMMER
 					WHERE t1.PERSONNUMMER is null
@@ -93,8 +113,8 @@ CREATE PROCEDURE [dbo].[usp_CPR_Broker_Compare] AS
 			-- Only if both temp tables contains data we carry out the comparison --
 			BEGIN
 				-- We look for updates in already existing records --
-				INSERT INTO T_E_MUpdateStaging (PERSONNUMMER, E_MTable, CreateTS) 
-					SELECT PERSONNUMMER, 'AA70300T', current_timestamp
+				INSERT INTO @T_E_MUpdateStaging_Tmp (PERSONNUMMER, E_MTable, CreateTS, Valid) 
+					SELECT PERSONNUMMER, 'AA70300T', current_timestamp, 0
 					FROM (
 						SELECT PERSONNUMMER, COUNT(*) AS PNR_COUNT  FROM (
 							SELECT * FROM AA70300T_TMP1 
@@ -106,13 +126,32 @@ CREATE PROCEDURE [dbo].[usp_CPR_Broker_Compare] AS
 					WHERE PNR_COUNT > 1
                 EXEC xp_logevent 70000, '... checked for differences...', informational
 				-- We look for new records --
-				INSERT INTO T_E_MUpdateStaging (PERSONNUMMER, E_MTable, CreateTS) 
-					SELECT t2.PERSONNUMMER, 'AA70300T', current_timestamp 
+				INSERT INTO @T_E_MUpdateStaging_Tmp (PERSONNUMMER, E_MTable, CreateTS, Valid) 
+					SELECT t2.PERSONNUMMER, 'AA70300T', current_timestamp, 0
 					FROM AA70300T_TMP2 t2 LEFT OUTER JOIN AA70300T_TMP1 t1
 					ON t2.PERSONNUMMER = t1.PERSONNUMMER
 					WHERE t1.PERSONNUMMER is null
                 EXEC xp_logevent 70000, '... checked for new records...', informational
 			END
+
+		
+		-- Mark the validity of the records
+		UPDATE T
+		SET Valid = 1
+		FROM @T_E_MUpdateStaging_Tmp T
+		INNER JOIN AA70000T P ON T.PERSONNUMMER = P.PERSONNUMMER
+		
+		-- Now insert either in staging table or excluded staging table
+		INSERT INTO T_E_MUpdateStaging (PERSONNUMMER, E_MTable, CreateTS)
+		SELECT PERSONNUMMER, E_MTable, CreateTS
+		FROM @T_E_MUpdateStaging_Tmp
+		WHERE Valid = 1
+
+		INSERT INTO T_E_MUpdateStaging_Excluded (PERSONNUMMER, E_MTable, CreateTS)
+		SELECT PERSONNUMMER, E_MTable, CreateTS
+		FROM @T_E_MUpdateStaging_Tmp
+		WHERE Valid = 0
+
 		-- Finally we truncate all the temp tables --
 		TRUNCATE TABLE AA70000T_TMP1;
 		TRUNCATE TABLE AA70300T_TMP1;
