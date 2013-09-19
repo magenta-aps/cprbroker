93c1ab4212b970032b8257c063df04278c52d1bc
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-09-19 13:32:02 +0000

    Added property ActualProductionUnits

---
+++ Cvr/Source/CvrDemo/Data/LegalUnit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CvrDemo.Data
+{
+    public partial class LegalUnit
+    {
+        public ProductionUnit[] ActualProductionUnits
+        {
+            get
+            {
+                return this.ProductionUnits.Where(pu => pu != null).ToArray();
+            }
+        }
+    }
+}
