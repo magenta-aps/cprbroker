0087ece970236a689db9b73c5f161bcc8d1cfd84
dennis <dennis@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-09-26 15:09:18 +0000

    Added a patch to create table DataProviderCall and added column ActivityID to table LogEntry. Also set the relation between the two tables.

---
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_3.sql
@@ -0,0 +1,9 @@
+﻿-- Create table DataProviderCall to keep track of calls made to dataproviders
+create table DataProviderCall (ID uniqueidentifier primary key, ActivityID uniqueidentifier not null, Time datetime, ProviderType varchar, Cost decimal, Operation varchar, Input varchar, Success bit)
+
+-- Add the field ActivityID to LogEntry in order to link it to rows in DataProviderCall
+alter table LogEntry add ActivityID uniqueidentifier
+
+-- Create the relation between the two tables
+ALTER TABLE [dbo].[LogEntry]  WITH CHECK ADD  CONSTRAINT [FK_LogEntry_DataProviderCall] FOREIGN KEY([ActivityID])
+REFERENCES [dbo].[DataProviderCall] ([ActivityID])
