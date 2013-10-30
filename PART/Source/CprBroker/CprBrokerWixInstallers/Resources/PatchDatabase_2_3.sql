08a42ce9e10dd44084e75540b64eeee2916d5b0b
dennis <dennis@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-10-30 11:46:36 +0000

    Forgotten commit of changes to the SQL patch for budget control and change in CprBrokerWixInstallers project file.

--- PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_3.sql
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_3.sql
@@ -1,5 +1,5 @@
 ﻿-- Create table DataProviderCall to keep track of calls made to dataproviders
-create table DataProviderCall (ID uniqueidentifier, ActivityID uniqueidentifier not null primary key, Time datetime, ProviderType varchar, Cost decimal, Operation varchar, Input varchar, Success bit)
+create table DataProviderCall (ID uniqueidentifier, ActivityID uniqueidentifier not null primary key, Time datetime, ProviderType varchar(255), Cost decimal, Operation varchar(255), Input varchar(255), Success bit)
 
 -- Add the field ActivityID to LogEntry in order to link it to rows in DataProviderCall
 alter table LogEntry add ActivityID uniqueidentifier
