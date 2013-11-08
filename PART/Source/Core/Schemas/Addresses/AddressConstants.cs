176d269b581892fd66f27a0fac3287dfe5fcaf82
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-11-08 10:33:13 +0000

    Constant for Greenlandic municipality codes

---
+++ PART/Source/Core/Schemas/Addresses/AddressConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.Schemas.Part
+{
+    public class AddressConstants
+    {
+        public const decimal GreenlandMunicipalCodeStart = 955;
+    }
+}
