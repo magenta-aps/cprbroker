9e20d0f436367ab7d4f58fceaa0f20c962bc393a
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2013-11-21 13:41:41 +0000

    Added new task to BatchClient to sync DPR databases directly without using web service calls

---
+++ PART/Source/CprBroker/BatchClient/SyncDPR.cs
@@ -0,0 +1,132 @@
+﻿/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The CPR Broker concept was initally developed by
+ * Gentofte Kommune / Municipality of Gentofte, Denmark.
+ * Contributor(s):
+ * Steen Deth
+ *
+ *
+ * The Initial Code for CPR Broker and related components is made in
+ * cooperation between Magenta, Gentofte Kommune and IT- og Telestyrelsen /
+ * Danish National IT and Telecom Agency
+ *
+ * Contributor(s):
+ * Beemen Beshara
+ * Niels Elgaard Larsen
+ * Leif Lodahl
+ * Steen Deth
+ *
+ * The code is currently governed by IT- og Telestyrelsen / Danish National
+ * IT and Telecom Agency
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Schemas;
+using CprBroker.Schemas.Part;
+using CprBroker.Utilities;
+using CprBroker.Utilities.ConsoleApps;
+using CprBroker.Engine;
+using CprBroker.Engine.Local;
+using CprBroker.Engine.Part;
+using CprBroker.Providers.DPR;
+
+namespace BatchClient
+{
+    public class SyncDPR : ConsoleEnvironment
+    {
+        UuidCache UuidCache = new UuidCache();
+        BrokerContext brokerContext;
+        public override string[] LoadCprNumbers()
+        {
+            Utilities.UpdateConnectionString(this.BrokerConnectionString);
+            BrokerContext.Initialize(this.ApplicationToken, "");
+            brokerContext = BrokerContext.Current;
+
+            string[] ret = null;
+
+            if (string.IsNullOrEmpty(SourceFile))
+            {
+                using (var dataContext = new DPRDataContext(OtherConnectionString))
+                {
+                    ret = dataContext.PersonTotals.Select(t => t.PNR).ToArray().Select(pnr => pnr.ToPnrDecimalString()).ToArray();
+                }
+            }
+            else
+            {
+                ret = Utilities.LoadCprNumbersOneByOne(SourceFile);
+            }
+
+            var allPnrs = new List<string>();
+            using (var dataContext = new DPRDataContext(OtherConnectionString))
+            {
+                allPnrs.AddRange(ret);
+                allPnrs.AddRange(dataContext.CivilStatus.Where(civ => civ.SpousePNR.HasValue).Select(civ => civ.SpousePNR).ToArray().Select(pnr => pnr.Value.ToPnrDecimalString()));
+                allPnrs.AddRange(dataContext.Childs.Where(ch => ch.ChildPNR.HasValue).Select(ch => ch.ChildPNR).ToArray().Select(pnr => pnr.Value.ToPnrDecimalString()));
+                allPnrs.AddRange(dataContext.PersonTotals.Select(t => t.FatherPersonalOrBirthdate).ToArray());
+                allPnrs.AddRange(dataContext.PersonTotals.Select(t => t.MotherPersonalOrBirthDate).ToArray());
+                allPnrs = allPnrs.Where(pnr => Strings.IsValidPersonNumber(pnr)).Distinct().ToList();
+
+                // fill by 1000 at a time
+                int startIndex = 0;
+                int batchSize = 500;
+                while (startIndex < allPnrs.Count)
+                {
+                    Log(string.Format("Getting UUIDs, starting at index <{0}>", startIndex));
+                    int lastIndex = startIndex + batchSize;
+                    lastIndex = Math.Min(lastIndex, allPnrs.Count);
+
+                    UuidCache.FillCache(allPnrs.Skip(startIndex).Take(lastIndex - startIndex).ToArray());
+                    startIndex += batchSize;
+                }
+            }
+            return ret;
+        }
+
+        public override void ProcessPerson(string pnr)
+        {
+            BrokerContext.Current = brokerContext;
+            try
+            {
+                using (var dprDataContext = new DPRDataContext(OtherConnectionString))
+                {
+                    var pId = new PersonIdentifier() { CprNumber = pnr, UUID = UuidCache.GetUuid(pnr) };
+                    var personInfo = PersonInfo.GetPersonInfo(dprDataContext, decimal.Parse(pnr));
+                    var reg = personInfo.ToRegisteringType1(UuidCache.GetUuid, dprDataContext);
+                    UpdateDatabase.UpdatePersonRegistration(pId, reg);
+                }
+            }
+            catch (Exception ex)
+            {
+                Admin.LogException(ex);
+                throw ex;
+            }
+        }
+    }
+}
