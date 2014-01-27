93a39433840f2592c7b9ae4329cf1e1fe79e17b1
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2014-01-27 12:23:27 +0000

    Enhanced version of Search caching SP

--- PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_2_2.sql
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_2_2_2.sql
@@ -65,7 +65,7 @@ IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'InitializePersonSearchCach
 	DROP PROCEDURE dbo.InitializePersonSearchCache
 GO
 
-CREATE PROCEDURE dbo.InitializePersonSearchCache
+CREATE PROCEDURE [dbo].[InitializePersonSearchCache]
 	@UUID UNIQUEIDENTIFIER, 
 	@PersonRegistrationId UNIQUEIDENTIFIER, 
 	@RegistrationDate DATETIME, 
@@ -95,15 +95,44 @@ BEGIN
 		UPDATE  PersonSearchCache
 		SET 
 			PersonRegistrationId = @PersonRegistrationId,
+			UserInterfaceKeyText = Convert(VARCHAR, tmp_UserInterfaceKeyText.Value.query('text()')),
+			BirthDate = Convert(VARCHAR, tmp_BirthDate.Value.query('text()')),
+			NickName = Convert(VARCHAR, tmp_NickName.Value.query('text()')),
+			Note = Convert(VARCHAR, tmp_Note.Value.query('text()')),
+			AddressingName = Convert(VARCHAR, tmp_AddressingName.Value.query('text()')),
 			PersonGivenName = CONVERT(VARCHAR, tmp_PersonGivenName.Value.query('text()')), 
 			PersonMiddleName = CONVERT(VARCHAR, tmp_PersonMiddleName.Value.query('text()')), 
 			PersonSurnameName = CONVERT(VARCHAR, tmp_PersonSurnameName.Value.query('text()')), 
 			PersonGenderCode = CONVERT(VARCHAR, tmp_PersonGenderCode.Value.query('text()')), 
-			LivscyklusKode = CONVERT(VARCHAR, tmp_LivscyklusKode.Value.query('text()')), 
-			BirthDate = Convert(VARCHAR, tmp_BirthDate.Value.query('text()'))
+			LivscyklusKode = CONVERT(VARCHAR, tmp_LivscyklusKode.Value.query('text()'))
 
 		FROM 
 			PersonSearchCache
+			
+			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
+			declare namespace ns1="http://rep.oio.dk/cpr.dk/xml/schemas/core/2005/03/18/";
+			(/ns0:Registrering/ns0:AttributListe/ns0:RegisterOplysning/ns0:CprBorger/ns1:PersonCivilRegistrationIdentifier)[last()]'
+			) AS tmp_UserInterfaceKeyText (Value)
+			
+			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
+			declare namespace ns1="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2005/03/15/";
+			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns1:BirthDate)[last()]'
+			) AS tmp_BirthDate (Value)
+			
+			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
+			declare namespace ns1="urn:oio:sagdok:2.0.0";
+			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:KaldenavnTekst)[last()]'
+			) AS tmp_NickName (Value)
+			
+			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
+			declare namespace ns1="urn:oio:sagdok:2.0.0";
+			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:NoteTekst)[last()]'
+			) AS tmp_Note (Value)
+			
+			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
+			declare namespace ns1="http://rep.oio.dk/itst.dk/xml/schemas/2005/02/22/";
+			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:PersonNameForAddressingName)[last()]'
+			) AS tmp_AddressingName (Value)
 			
 			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
 			declare namespace ns1="http://rep.oio.dk/itst.dk/xml/schemas/2006/01/17/";
@@ -133,10 +162,6 @@ BEGIN
 			(/ns0:Registrering/ns1:LivscyklusKode)[last()]'
 			) AS tmp_LivscyklusKode (Value)
 
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2005/03/15/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns1:BirthDate)[last()]'
-			) AS tmp_BirthDate (Value)
 			
 
 		WHERE
