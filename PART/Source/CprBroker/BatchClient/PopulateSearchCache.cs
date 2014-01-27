d6a168528aa46cb7e9cf7358872fa6c4d4dc7d94
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2014-01-27 12:56:20 +0000

    New task in BatchClient: PopulateSearchCache

---
+++ PART/Source/CprBroker/BatchClient/PopulateSearchCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CprBroker.Utilities.ConsoleApps;
+
+namespace BatchClient
+{
+    class PopulateSearchCache : ConsoleEnvironment
+    {
+        public override string[] LoadCprNumbers()
+        {
+            using (var dataContext = new CprBroker.Data.Part.PartDataContext(BrokerConnectionString))
+            {
+                return dataContext.PersonRegistrations
+                    .Select(pr => pr.UUID.ToString()).Distinct().OrderBy(pr => pr).ToArray();
+            }
+        }
+
+        public override void ProcessPerson(string uuid)
+        {
+            using (var dataContext = new CprBroker.Data.Part.PartDataContext(BrokerConnectionString))
+            {
+                var reg = dataContext.PersonRegistrations.Where(pr => pr.UUID == new Guid(uuid)).OrderByDescending(pr => pr.RegistrationDate).First();
+                dataContext.ExecuteCommand("UPDATE PersonRegistration SET Contents = Contents WHERE UUID={0} AND PersonRegistrationId = {1}", uuid, reg.PersonRegistrationId);
+            }
+        }
+    }
+}
