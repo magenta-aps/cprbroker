1042580a1c4461b857d1e04613f2cbb03a5041b8
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2014-02-18 14:55:48 +0000

    Support for abstract classes

--- PART/Source/Lib/SchemaGeneration/SchemaGeneration/SplitCodeBySchemaSource.cs
+++ PART/Source/Lib/SchemaGeneration/SchemaGeneration/SplitCodeBySchemaSource.cs
@@ -73,7 +73,7 @@ namespace SchemaGeneration
             string typePattern = ""
                 + @"(^\s{4}///.+\r\n)*" // XML doc line
                 + @"(^\s{4}\[.+\]\r\n)*"
-                + @"^\s{4}public (partial\s)?(class|enum)\s+(?<typeName>\w+).+\r\n"
+                + @"^\s{4}public ((abstract|partial)\s)*(class|enum)\s+(?<typeName>\w+).+\r\n"
                 + @"(^\s{8}.*\r\n)*"
                 + @"^\s{4}\}\r\n"
                 + @"";
