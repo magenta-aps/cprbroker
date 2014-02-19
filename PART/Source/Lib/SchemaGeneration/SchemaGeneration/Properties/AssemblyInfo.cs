15325986847b13d0a8ddb558c3f43491f21e87d0
beemen <beemen@6c425ffd-00ce-3b44-a1fb-8caf02015aca>
2014-02-19 12:12:03 +0000

    ComVisible = true

--- PART/Source/Lib/SchemaGeneration/SchemaGeneration/Properties/AssemblyInfo.cs
+++ PART/Source/Lib/SchemaGeneration/SchemaGeneration/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("979db8a8-36b2-4e7b-8837-c1cf98b6806a")]
