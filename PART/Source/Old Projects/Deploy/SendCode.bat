1d81ad52c2fe6c02d5eca72e2771d390fbaf4136
Beemen Beshara <beemen@magenta-aps.dk>
2014-04-24 17:31:52 +0200

    Removed folder with old projects

--- PART/Source/Old Projects/Deploy/SendCode.bat
+++
@@ -1,4 +0,0 @@
-rd /S /Q Deploy\Source
-xcopy Source\*.* Deploy\Source\*.* /E /Y /C /Q /Exclude:Deploy\excl.txt
-del Deploy\Source.7z /q
-"C:\Program Files\7-Zip\7z.exe" a Deploy\Source.7z Deploy\Source\
