191febebe1c521f91a593c91068ff94701ee9d8c
beemen <beemen@magenta-aps.dk>
2014-04-25 15:19:02 +0200

    Initial dummy implementation of CPRDirect to DPR conversion

---
+++ PART/Source/CprBroker/CPRDirectToDPR/CprConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Providers.DPR;
+using CprBroker.Providers.CPRDirect;
+
+namespace CPRDirectToDPR
+{
+    public class CprConverter
+    {
+        public void AppendPerson(IndividualResponseType person, DPRDataContext dataContext)
+        {
+            dataContext.PersonTotals.InsertOnSubmit(person.ToPersonTotal());
+            
+            dataContext.Childs.InsertAllOnSubmit(person.Child.Select(c=>c.ToDpr()));
+            
+            dataContext.PersonNames.InsertOnSubmit(person.CurrentNameInformation.ToDpr());
+            dataContext.PersonNames.InsertAllOnSubmit(person.HistoricalName.Select(n=>n.ToDpr()));
+            
+            dataContext.CivilStatus.InsertOnSubmit(person.CurrentCivilStatus.ToDpr());
+            dataContext.CivilStatus.InsertAllOnSubmit(person.HistoricalCivilStatus.Select(c => c.ToDpr()));
+
+            dataContext.Separations.InsertOnSubmit(person.CurrentSeparation.ToDpr());
+            dataContext.Separations.InsertAllOnSubmit(person.HistoricalSeparation.Select(c => c.ToDpr()));
+
+            dataContext.Nationalities.InsertOnSubmit(person.CurrentCitizenship.ToDpr());
+            dataContext.Nationalities.InsertAllOnSubmit(person.HistoricalCitizenship.Select(c => c.ToDpr()));
+
+            dataContext.Departures.InsertOnSubmit(person.CurrentDepartureData.ToDpr());
+            dataContext.Departures.InsertAllOnSubmit(person.HistoricalDeparture.Select(c => c.ToDpr()));
+
+            dataContext.ContactAddresses.InsertOnSubmit(person.ContactAddress.ToDpr());
+
+            var currenrAddress = person.GetFolkeregisterAdresseSource(false) as CurrentAddressWrapper;
+            if(currenrAddress != null)
+                dataContext.PersonAddresses.InsertOnSubmit(currenrAddress.ToDpr());
+            dataContext.CivilStatus.InsertAllOnSubmit(person.HistoricalCivilStatus.Select(c => c.ToDpr()));
+        }
+    }
+}
---
+++ PART/Source/CprBroker/CPRDirectToDPR/CprConverterExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Providers.DPR;
+using CprBroker.Providers.CPRDirect;
+
+namespace CPRDirectToDPR
+{
+    public static class CprConverterExtensions
+    {
+        public static PersonTotal ToPersonTotal(this IndividualResponseType resp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Child ToDpr(this ChildType child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static PersonName ToDpr(this CurrentNameInformationType currentName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static PersonName ToDpr(this HistoricalNameType historicalName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static CivilStatus ToDpr(this CurrentCivilStatusType currentCivilStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static CivilStatus ToDpr(this HistoricalCivilStatusType historicalCivilStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Separation ToDpr(this CurrentSeparationType currentSeparation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Separation ToDpr(this HistoricalSeparationType historicalSeparation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Nationality ToDpr(this CurrentCitizenshipType currentCitizenship)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Nationality ToDpr(this HistoricalCitizenshipType historicalCitizenship)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Departure ToDpr(this CurrentDepartureDataType currentDeparture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Departure ToDpr(this HistoricalDepartureType historicalDeparture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ContactAddress ToDpr(this ContactAddressType contactAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static PersonAddress ToDpr(this CurrentAddressWrapper currentAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static PersonAddress ToDpr(this HistoricalAddressType historicalAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+    }
+}
