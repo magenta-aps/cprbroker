2f30948626eab4ed7a70be9bedeb0b196eeaaa05
beemen <beemen@magenta-aps.dk>
2014-04-30 14:01:02 +0200

    Initial implementation of generic sync logic

---
+++ PART/Source/Core/Engine/Sync/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.Engine.Sync
+{
+    public interface IDataStore<TKey>
+    {
+
+    }
+
+    public interface ISourceDataStore<TKey, TObject> : IDataStore<TKey>
+    {
+        TKey[] GetChangeKeys(int maxCount = 1);
+        TObject[] GetObjects(TKey[] keys);
+        void DeleteChanges(TKey[] changes);
+    }
+
+    public interface ITargetDataStore<TKey, TObject> : IDataStore<TKey>
+    {
+        void PushChanges(TKey[] changes, TObject[] objects);
+    }
+
+}
---
+++ PART/Source/Core/Engine/Sync/SyncManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.Engine.Sync
+{
+    public abstract class SyncManager<TKey, TSourceObject, TTargetObject, TSourceStore, TTargetStore>
+        where TSourceObject : ISourceDataStore<TKey, TSourceObject>
+        where TTargetObject : ITargetDataStore<TKey, TTargetObject>
+    {
+        public int ConvertBatch(TSourceObject source, TTargetObject target, Converter<TSourceObject, TTargetObject> converter, int batchSize)
+        {
+            TKey[] keys = source.GetChangeKeys(batchSize);
+            Local.Admin.LogFormattedSuccess("<{0}> changes found at store <{1}>, first = <{2}>", keys.Length, source, keys.FirstOrDefault());
+            TSourceObject[] sourceObjects = source.GetObjects(keys);
+            TTargetObject[] targetObjects = Array.ConvertAll<TSourceObject, TTargetObject>(sourceObjects, converter);
+            target.PushChanges(keys, targetObjects);
+            source.DeleteChanges(keys);
+            Local.Admin.LogFormattedSuccess("Done pushing <{0}> changes to store <{1}>", keys.Length, target);
+
+            return keys.Length;
+        }
+
+        public int ConvertAll(TSourceObject source, TTargetObject target, Converter<TSourceObject, TTargetObject> converter, int batchSize, int maxItems)
+        {
+            int convertedSoFar = 0;
+            bool run = true;
+
+            Local.Admin.LogFormattedSuccess("Requesting store <{0}> for <{1}> changes", source, batchSize);
+            
+            while (run && convertedSoFar < maxItems)
+            {
+                int myBatchSize = Math.Min(batchSize, maxItems-convertedSoFar);
+                convertedSoFar += ConvertBatch(source, target, converter, myBatchSize);
+            }
+            return convertedSoFar;
+        }
+    }
+}
