b3c514045485748047569d288fc554c2a0ae0a93
beemen <beemen@magenta-aps.dk>
2014-05-02 16:20:00 +0200

    Added missing file

---
+++ PART/Source/Core/Data/Queues/QueueItemBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.Data.Queues
+{
+    public abstract class QueueItemBase : IQueueItem
+    {
+        public virtual QueueItem Impl { get; set; }
+        public abstract string SerializeToKey();
+        public abstract void DeserializeFromKey(string key);
+    }
+}
