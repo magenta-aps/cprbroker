abec49dac21be73fc2aa69d443f505025c4ed504
beemen <beemen@magenta-aps.dk>
2014-05-14 13:07:39 +0200

    Namespace adjustment

--- PART/Source/EventBroker/EventBroker/Notifications/QueueExecuter.cs
+++ PART/Source/EventBroker/EventBroker/Notifications/QueueExecuter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CprBroker.Data.Queues;
+using CprBroker.Engine.Queues;
 
 
 namespace CprBroker.EventBroker.Notifications
