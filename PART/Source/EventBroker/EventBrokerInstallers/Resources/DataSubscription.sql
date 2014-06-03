a451831136e487d62389b4823baf0024e91c1c5e
beemen <beemen@magenta-aps.dk>
2014-06-03 15:30:48 +0200

    Short formt for TSQL for DataSubscription

--- PART/Source/EventBroker/EventBrokerInstallers/Resources/DataSubscription.sql
+++ PART/Source/EventBroker/EventBrokerInstallers/Resources/DataSubscription.sql
@@ -1,12 +1,9 @@
-﻿/****** Object:  ForeignKey [FK_DataSubscription_Subscription]    Script Date: 02/13/2011 17:59:04 ******/
-IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_DataSubscription_Subscription]') AND parent_object_id = OBJECT_ID(N'[dbo].[DataSubscription]'))
-ALTER TABLE [dbo].[DataSubscription] DROP CONSTRAINT [FK_DataSubscription_Subscription]
-GO
-/****** Object:  Table [dbo].[DataSubscription]    Script Date: 02/13/2011 17:59:04 ******/
+﻿/****** Object:  Table [dbo].[DataSubscription] ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DataSubscription]') AND type in (N'U'))
 DROP TABLE [dbo].[DataSubscription]
 GO
-/****** Object:  Table [dbo].[DataSubscription]    Script Date: 02/13/2011 17:59:04 ******/
+
+/****** Object:  Table [dbo].[DataSubscription] ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -15,19 +12,8 @@ IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Da
 BEGIN
 CREATE TABLE [dbo].[DataSubscription](
 	[SubscriptionId] [uniqueidentifier] NOT NULL,
- CONSTRAINT [PK_DataSubscription] PRIMARY KEY CLUSTERED 
-(
-	[SubscriptionId] ASC
-)WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF)
+    CONSTRAINT [PK_DataSubscription] PRIMARY KEY CLUSTERED ([SubscriptionId] ASC),
+    CONSTRAINT [FK_DataSubscription_Subscription] FOREIGN KEY([SubscriptionId]) REFERENCES [dbo].[Subscription] ([SubscriptionId]) ON UPDATE CASCADE ON DELETE CASCADE
 )
 END
-GO
-/****** Object:  ForeignKey [FK_DataSubscription_Subscription]    Script Date: 02/13/2011 17:59:04 ******/
-IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_DataSubscription_Subscription]') AND parent_object_id = OBJECT_ID(N'[dbo].[DataSubscription]'))
-ALTER TABLE [dbo].[DataSubscription]  WITH CHECK ADD  CONSTRAINT [FK_DataSubscription_Subscription] FOREIGN KEY([SubscriptionId])
-REFERENCES [dbo].[Subscription] ([SubscriptionId])
-ON UPDATE CASCADE
-ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[DataSubscription] CHECK CONSTRAINT [FK_DataSubscription_Subscription]
 GO
