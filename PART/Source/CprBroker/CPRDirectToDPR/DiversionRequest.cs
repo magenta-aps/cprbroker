34d6a68264475b59cff0fb93e8f8d88a01b3f0c1
beemen <beemen@magenta-aps.dk>
2014-06-12 16:57:51 +0200

    First implementation of DprDiversionServer - Inherits from TcpServer - XML definition of wrappers - Initial classes

---
+++ PART/Source/CprBroker/CPRDirectToDPR/DiversionRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Providers.DPR;
+
+namespace CprBroker.DBR
+{
+    public class DiversionRequest
+    {
+        public InquiryType inquiryType;
+        public DetailType detailType;
+        public string cprNumber;
+
+        public static DiversionRequest Parse(byte[] message)
+        {
+            return Parse(Constants.DiversionEncoding.GetString(message));
+        }
+
+        public static DiversionRequest Parse(string str)
+        {
+            DiversionRequest ret = null;
+            if (str.Length == 12)
+            {
+                ret = new DiversionRequest()
+                {
+                    inquiryType = (InquiryType)int.Parse(str[0].ToString()),
+                    detailType = (DetailType)int.Parse(str[1].ToString()),
+                    cprNumber = str.Substring(2),
+                };
+            }
+            else if (str.Length == 40)
+            {
+                ret = new DiversionRequestV3()
+                {
+                    inquiryType = (InquiryType)int.Parse(str[0].ToString()),
+                    detailType = (DetailType)int.Parse(str[1].ToString()),
+                    cprNumber = str.Substring(2),
+                    
+                };
+            }
+            return ret;
+        }
+
+        public virtual string Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    public class DiversionRequestV3 : DiversionRequest
+    {
+
+        public override string Process()
+        {
+            return base.Process();
+        }
+    }
+}
