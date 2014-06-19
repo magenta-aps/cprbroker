bff3d9b9650c709897b7c60f5064813718aa60c8
beemen <beemen@magenta-aps.dk>
2014-06-19 10:39:31 +0200

    New class for NewRequestType

---
+++ PART/Source/CprBroker/CPRDirectToDPR/NewRquestType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.DBR
+{
+    public partial class NewRquestType
+    {
+        public override DiversionResponse Process(string dprConnectionString)
+        {
+            // TODO: handle the new request format
+            throw new NotImplementedException();
+        }
+    }
+}
