89fb21f430a22b4d1e5c5f61d3db762fa97c684b
beemen <beemen@magenta-aps.dk>
2014-06-23 10:22:13 +0200

    New (empty) file for lookup conversions

---
+++ PART/Source/CprBroker/DBR/CprConverterExtensions.Lookups.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Providers.DPR;
+using CprBroker.Providers.CPRDirect;
+
+namespace CprBroker.DBR
+{
+    public static partial class CprConverterExtensions
+    {
+
+    }
+}
