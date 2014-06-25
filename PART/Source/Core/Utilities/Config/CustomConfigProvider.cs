309ae3cd57a2b63d001efb93df68551cf30aaf5c
beemen <beemen@magenta-aps.dk>
2014-06-25 10:43:01 +0200

    Draft implementation of custom config provider

---
+++ PART/Source/Core/Utilities/Config/CustomConfigProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace CprBroker.Config
+{
+    class CustomConfigProvider : IConfigProvider
+    {
+        public Properties.Settings Settings
+        {
+            get 
+            {
+                var ret = new Properties.Settings();
+                return ret;
+            }
+        }
+
+        public System.Security.Cryptography.RijndaelManaged EncryptionAlgorithm
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Engine.DataProvidersConfigurationSection DataProvidersSection
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
