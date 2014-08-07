5f18d2848bd46b9c53df392ec322ed86430fa20e
beemen <beemen@magenta-aps.dk>
2014-08-07 16:37:32 +0200

    Untested version of InitializePersonSearchCache with the new format - Checking of currently active properties (effect end date = null) - Split over logical groups - Update/Insert reduced to one atomic statement

--- PART/Source/CprBroker/CprBrokerWixInstallers/Resources/InitializePersonSearchCache.sql
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/InitializePersonSearchCache.sql
@@ -5,111 +5,129 @@
 -- Description:	Initializes the cashed version of persons' searchable fields
 -- =============================================
 IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'InitializePersonSearchCache')
-	DROP PROCEDURE dbo.InitializePersonSearchCache
+    DROP PROCEDURE dbo.InitializePersonSearchCache
 GO
 
 CREATE PROCEDURE [dbo].[InitializePersonSearchCache]
-	@UUID UNIQUEIDENTIFIER, 
-	@PersonRegistrationId UNIQUEIDENTIFIER, 
-	@RegistrationDate DATETIME, 
-	@Contents XML
+    @UUID UNIQUEIDENTIFIER, 
+    @PersonRegistrationId UNIQUEIDENTIFIER, 
+    @RegistrationDate DATETIME, 
+    @Contents XML
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
-	SET NOCOUNT ON;
+    -- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
+    SET NOCOUNT ON;
     
     -- Determine if this is the latest registration
-	IF NOT EXISTS (
-		SELECT PersonRegistrationId 
-		FROM PersonRegistration 
-		WHERE 
-			UUID = @UUID 
-			AND RegistrationDate > @RegistrationDate
-		)
-	BEGIN
-		-- Ensure record exists
-		IF NOT EXISTS (SELECT UUID FROM PersonSearchCache WHERE UUID = @UUID)
-		BEGIN
-			DECLARE @C INT		
-			INSERT INTO PersonSearchCache(UUID) VALUES (@UUID)
-		END
-		
-		-- Fill the data
-		UPDATE  PersonSearchCache
-		SET 
-			PersonRegistrationId = @PersonRegistrationId,
-			UserInterfaceKeyText = Convert(VARCHAR(MAX), tmp_UserInterfaceKeyText.Value.query('text()')),
-			BirthDate = Convert(VARCHAR(MAX), tmp_BirthDate.Value.query('text()')),
-			NickName = Convert(VARCHAR(MAX), tmp_NickName.Value.query('text()')),
-			Note = Convert(VARCHAR(MAX), tmp_Note.Value.query('text()')),
-			AddressingName = Convert(VARCHAR(MAX), tmp_AddressingName.Value.query('text()')),
-			PersonGivenName = CONVERT(VARCHAR(MAX), tmp_PersonGivenName.Value.query('text()')), 
-			PersonMiddleName = CONVERT(VARCHAR(MAX), tmp_PersonMiddleName.Value.query('text()')), 
-			PersonSurnameName = CONVERT(VARCHAR(MAX), tmp_PersonSurnameName.Value.query('text()')), 
-			PersonGenderCode = CONVERT(VARCHAR(MAX), tmp_PersonGenderCode.Value.query('text()')), 
-			LivscyklusKode = CONVERT(VARCHAR(MAX), tmp_LivscyklusKode.Value.query('text()'))
+    -- If not, quit
+    IF EXISTS (
+        SELECT PersonRegistrationId 
+        FROM PersonRegistration 
+        WHERE 
+            UUID = @UUID 
+            AND RegistrationDate > @RegistrationDate
+        )
+    BEGIN
+        RETURN
+    END
+        
+    -------------------
+    -- Root variables
+    -------------------
+    DECLARE @ContentsTable Table (X XML)
+    DECLARE @LivscyklusKode VARCHAR(MAX);
+    DECLARE @EgenskabNode XML, @RegisterOplysningNode XML;
+    
+    INSERT INTO @ContentsTable SELECT @Contents;
 
-		FROM 
-			PersonSearchCache
-			
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/cpr.dk/xml/schemas/core/2005/03/18/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:RegisterOplysning/ns0:CprBorger/ns1:PersonCivilRegistrationIdentifier)[last()]'
-			) AS tmp_UserInterfaceKeyText (Value)
-			
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2005/03/15/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns1:BirthDate)[last()]'
-			) AS tmp_BirthDate (Value)
-			
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="urn:oio:sagdok:2.0.0";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:KaldenavnTekst)[last()]'
-			) AS tmp_NickName (Value)
-			
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="urn:oio:sagdok:2.0.0";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:NoteTekst)[last()]'
-			) AS tmp_Note (Value)
-			
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/itst.dk/xml/schemas/2005/02/22/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:PersonNameForAddressingName)[last()]'
-			) AS tmp_AddressingName (Value)
-			
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/itst.dk/xml/schemas/2006/01/17/";
-			declare namespace ns2="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2003/02/13/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:PersonNameStructure/ns2:PersonGivenName)[last()]'
-			) AS tmp_PersonGivenName (Value)
+    WITH XMLNAMESPACES(
+        'urn:oio:sagdok:person:1.0.0' as ns0,
+        'urn:oio:sagdok:2.0.0' as ns1 )
+    SELECT 
+        @EgenskabNode = X.query('(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab[ns1:Virkning/ns1:TilTidspunkt/ns1:GraenseIndikator])'),
+        @RegisterOplysningNode = X.query('(/ns0:Registrering/ns0:AttributListe/ns0:RegisterOplysning[ns1:Virkning/ns1:TilTidspunkt/ns1:GraenseIndikator])'),
+        @LivscyklusKode = X.value('(/ns0:Registrering/ns1:LivscyklusKode)[last()]','varchar(max)')
+    FROM 
+        @ContentsTable
 
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/itst.dk/xml/schemas/2006/01/17/";
-			declare namespace ns2="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2003/02/13/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:PersonNameStructure/ns2:PersonMiddleName)[last()]'
-			) AS tmp_PersonMiddleName (Value)
+    --------------
+    -- Egenskab --
+    --------------
+    DECLARE @AddressingName VARCHAR(MAX), @NickName VARCHAR(MAX), @Note VARCHAR(MAX), 
+        @PersonGivenName VARCHAR(MAX), @PersonMiddleName VARCHAR(MAX), @PersonSurnameName VARCHAR(MAX), 
+        @PersonGenderCode VARCHAR(MAX), @Birthdate VARCHAR(MAX)
 
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/itst.dk/xml/schemas/2006/01/17/";
-			declare namespace ns2="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2003/02/13/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns0:NavnStruktur/ns1:PersonNameStructure/ns2:PersonSurnameName)[last()]'
-			) AS tmp_PersonSurnameName (Value)
+    IF LEN (CAST (@EgenskabNode AS VARCHAR(MAX))) > 0
+    BEGIN	
+        DECLARE @EgenskabTable TABLE (X XML);
+        INSERT INTO @EgenskabTable SELECT @EgenskabNode;
+    
+        WITH XMLNAMESPACES (
+            'urn:oio:sagdok:person:1.0.0' as ns0,
+            'http://rep.oio.dk/itst.dk/xml/schemas/2005/02/22/' as ns1,
+            'urn:oio:sagdok:2.0.0' as ns2,
+            'http://rep.oio.dk/itst.dk/xml/schemas/2006/01/17/' as ns3,
+            'http://rep.oio.dk/ebxml/xml/schemas/dkcc/2003/02/13/' as ns4,
+            'http://rep.oio.dk/ebxml/xml/schemas/dkcc/2006/01/23/' as ns5,
+            'http://rep.oio.dk/ebxml/xml/schemas/dkcc/2005/03/15/' as ns6) 
+        SELECT 
+            @AddressingName = X.value('(/ns0:Egenskab/ns0:NavnStruktur/ns1:PersonNameForAddressingName)[last()]','varchar(max)'),
+            @NickName = X.value('(/ns0:Egenskab/ns0:NavnStruktur/ns2:KaldenavnTekst)[last()]','varchar(max)'),
+            @Note = X.value('(/ns0:Egenskab/ns0:NavnStruktur/ns2:NoteTekst)[last()]','varchar(max)'),		
+            @PersonGivenName = X.value('(/ns0:Egenskab/ns0:NavnStruktur/ns3:PersonNameStructure/ns4:PersonGivenName)[last()]','varchar(max)'),
+            @PersonMiddleName = X.value('(/ns0:Egenskab/ns0:NavnStruktur/ns3:PersonNameStructure/ns4:PersonMiddleName)[last()]','varchar(max)'),
+            @PersonSurnameName = X.value('(/ns0:Egenskab/ns0:NavnStruktur/ns3:PersonNameStructure/ns4:PersonSurnameName)[last()]','varchar(max)'),
+            @PersonGenderCode = X.value('(/ns0:Egenskab/ns5:PersonGenderCode)[last()]','varchar(max)'),
+            @Birthdate = X.value('(/ns0:Egenskab/ns6:BirthDate)[last()]','varchar(max)')
+        FROM 
+            @EgenskabTable
+    END
 
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="http://rep.oio.dk/ebxml/xml/schemas/dkcc/2006/01/23/";
-			(/ns0:Registrering/ns0:AttributListe/ns0:Egenskab/ns1:PersonGenderCode)[last()]'
-			) AS tmp_PersonGenderCode (Value)
+    -----------------------
+    -- RegisterOplysning --
+    -----------------------
 
-			OUTER APPLY @Contents.nodes ('declare namespace ns0="urn:oio:sagdok:person:1.0.0";
-			declare namespace ns1="urn:oio:sagdok:2.0.0";
-			(/ns0:Registrering/ns1:LivscyklusKode)[last()]'
-			) AS tmp_LivscyklusKode (Value)
+    DECLARE @UserInterfaceKeyText VARCHAR(MAX);
+    IF LEN (CAST (@RegisterOplysningNode AS VARCHAR(MAX))) > 0
+    BEGIN	
+        DECLARE @RegisterOplysningTable TABLE (X XML);
+        INSERT INTO @RegisterOplysningTable SELECT @RegisterOplysningNode;
+    
+        WITH XMLNAMESPACES (
+            'urn:oio:sagdok:person:1.0.0' as ns0,
+            'http://rep.oio.dk/cpr.dk/xml/schemas/core/2005/03/18/' as ns1)
+        SELECT
+            @UserInterfaceKeyText = X.value('(/ns0:RegisterOplysning/ns0:CprBorger/ns1:PersonCivilRegistrationIdentifier)[last()]','varchar(max)')
+        FROM @RegisterOplysningTable
+    END
 
-			
-
-		WHERE
-			UUID = @UUID
-	END
-		
+    -----------------------------
+    -- INSERT OR UPDATE RECORD --
+    -----------------------------
+    IF EXISTS (SELECT UUID FROM PersonSearchCache WHERE UUID = @UUID)
+    BEGIN			
+        UPDATE PersonSearchCache 
+        SET PersonRegistrationId = @PersonRegistrationId, LivscyklusKode = @LivscyklusKode,	
+            AddressingName = @AddressingName, NickName = @NickName, Note = @Note, 
+            PersonGivenName = @PersonGivenName, PersonMiddleName = @PersonMiddleName, PersonSurnameName = @PersonSurnameName, 
+            PersonGenderCode = @PersonGenderCode, Birthdate = @Birthdate,
+            UserInterfaceKeyText = @UserInterfaceKeyText
+        WHERE 
+            UUID = @UUID
+    END
+    ELSE
+    BEGIN
+        INSERT INTO PersonSearchCache (
+            UUID, PersonRegistrationId, LivscyklusKode, 
+            AddressingName, NickName, Note, 
+            PersonGivenName, PersonMiddleName, PersonSurnameName, 
+            PersonGenderCode, Birthdate, 
+            UserInterfaceKeyText)
+        VALUES (@UUID, @PersonRegistrationId, @LivscyklusKode, 
+            @AddressingName, @NickName, @Note, 
+            @PersonGivenName, @PersonMiddleName, @PersonSurnameName, 
+            @PersonGenderCode, @Birthdate, 
+            @UserInterfaceKeyText)	    	        
+    END
 END
 GO
