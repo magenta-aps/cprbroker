614f64df505328d1880a77bf3dbd667915db4235
beemen <beemen@magenta-aps.dk>
2014-09-18 14:29:29 +0200

    Creation of PersonSearchCache table

--- PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PersonSearchCache.sql
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PersonSearchCache.sql
@@ -4,18 +4,39 @@
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE Name = 'PersonSearchCache')
 BEGIN
 	CREATE TABLE [dbo].[PersonSearchCache](
-		[PersonRegistrationId] [uniqueidentifier] NULL,
-		[UUID] [uniqueidentifier] NOT NULL,
-		[UserInterfaceKeyText] [varchar](max) NULL,
-		[Birthdate] [varchar](max) NULL,
-		[NickName] [varchar](max) NULL,
-		[Note] [varchar](max) NULL,
-		[AddressingName] [varchar](max) NULL,
-		[PersonGivenName] [varchar](max) NULL,
-		[PersonMiddleName] [varchar](max) NULL,
-		[PersonSurnameName] [varchar](max) NULL,
-		[PersonGenderCode] [varchar](max) NULL,
-		[LivscyklusKode] [varchar](max) NULL,
+		-- Root fields
+        
+        [PersonRegistrationId] [uniqueidentifier] NULL,
+		[UUID] [uniqueidentifier] NOT NULL,		
+        LivscyklusKode VARCHAR(MAX),
+            
+        -- Egenskab fields
+        AddressingName VARCHAR(MAX), NickName VARCHAR(MAX), Note VARCHAR(MAX), 
+        PersonGivenName VARCHAR(MAX), PersonMiddleName VARCHAR(MAX), PersonSurnameName VARCHAR(MAX), 
+        PersonGenderCode VARCHAR(MAX), Birthdate VARCHAR(MAX), 
+            
+        -- CprBorger
+        UserInterfaceKeyText VARCHAR(MAX), PersonCivilRegistrationIdentifier VARCHAR(MAX),
+        PersonNummerGyldighedStatusIndikator VARCHAR(MAX), PersonNationalityCode VARCHAR(MAX),
+        NavneAdresseBeskyttelseIndikator VARCHAR(MAX), TelefonNummerBeskyttelseIndikator VARCHAR(MAX), ForskerBeskyttelseIndikator VARCHAR(MAX),
+            
+        -- CprBorger - after address
+        AdresseNoteTekst VARCHAR(MAX), FolkekirkeMedlemIndikator VARCHAR(MAX),
+
+        -- FolkeregisterAdresse
+        NoteTekst_DanskAdresse VARCHAR(MAX), UkendtAdresseIndikator VARCHAR(MAX),
+        SpecielVejkodeIndikator VARCHAR(MAX), 
+
+        -- AddressAccess
+        MunicipalityCode VARCHAR(MAX), StreetCode VARCHAR(MAX), StreetBuildingIdentifier VARCHAR(MAX),
+
+        -- AddressPostal
+        MailDeliverySublocationIdentifier VARCHAR(MAX), StreetName VARCHAR(MAX), StreetNameForAddressingName VARCHAR(MAX),
+        StreetBuildingIdentifier_Postal VARCHAR(MAX), FloorIdentifier VARCHAR(MAX), SuiteIdentifier VARCHAR(MAX),
+        DistrictSubdivisionIdentifier VARCHAR(MAX), PostOfficeBoxIdentifier VARCHAR(MAX),
+        PostCodeIdentifier VARCHAR(MAX), DistrictName VARCHAR(MAX),
+        CountryIdentificationCode VARCHAR(MAX)
+
 	 CONSTRAINT [PK_PersonSearchCache] PRIMARY KEY CLUSTERED 
 	(
 		[UUID] ASC
