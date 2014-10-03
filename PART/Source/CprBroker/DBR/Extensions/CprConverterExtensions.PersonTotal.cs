b6d62fd5b05f1d2ff801e515c88300281727065c
beemen <beemen@magenta-aps.dk>
2014-10-03 14:12:41 +0200

    Added license text to DBR project

--- PART/Source/CprBroker/DBR/Extensions/CprConverterExtensions.PersonTotal.cs
+++ PART/Source/CprBroker/DBR/Extensions/CprConverterExtensions.PersonTotal.cs
@@ -1,4 +1,48 @@
-﻿using System;
+﻿/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The CPR Broker concept was initally developed by
+ * Gentofte Kommune / Municipality of Gentofte, Denmark.
+ * Contributor(s):
+ * Steen Deth
+ *
+ *
+ * The Initial Code for CPR Broker and related components is made in
+ * cooperation between Magenta, Gentofte Kommune and IT- og Telestyrelsen /
+ * Danish National IT and Telecom Agency
+ *
+ * Contributor(s):
+ * Beemen Beshara
+ * Dennis Amdi Skov Isaksen
+ *
+ * The code is currently governed by IT- og Telestyrelsen / Danish National
+ * IT and Telecom Agency
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -184,12 +228,14 @@ namespace CprBroker.DBR.Extensions
             var prevAddress = previousAddresses.FirstOrDefault();
             if (prevAddress != null)
             {
-                pt.PreviousAddress = string.Format("{0} {1},{2} {3} ({4})",
-                                        Street.GetAddressingName(dataContext.Connection.ConnectionString, prevAddress.MunicipalityCode, prevAddress.StreetCode),
-                                        prevAddress.HouseNumber.TrimStart('0', ' '),
-                                        prevAddress.Floor.TrimStart('0', ' '),
-                                        prevAddress.Door.TrimStart('0', ' '),
-                                        Authority.GetAuthorityNameByCode(prevAddress.MunicipalityCode.ToString()));
+                pt.PreviousAddress = string
+                    .Format("{0} {1},{2} {3} ({4})",
+                        Street.GetAddressingName(dataContext.Connection.ConnectionString, prevAddress.MunicipalityCode, prevAddress.StreetCode),
+                        prevAddress.HouseNumber.TrimStart('0', ' '),
+                        prevAddress.Floor.TrimStart('0', ' '),
+                        prevAddress.Door.TrimStart('0', ' '),
+                        Authority.GetAuthorityNameByCode(prevAddress.MunicipalityCode.ToString())
+                    ).Trim();
                 if (string.IsNullOrEmpty(pt.CurrentMunicipalityName))
                     pt.CurrentMunicipalityName = Authority.GetAuthorityNameByCode(prevAddress.MunicipalityCode.ToString());
             }
