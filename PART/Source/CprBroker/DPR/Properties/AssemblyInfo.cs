b4e9adbc744ce83c588da88bb0b4a1f59869792e
beemen <beemen@magenta-aps.dk>
2014-11-24 14:54:33 +0100

    Upgraded version numbers in CprBroker solution to 2.2.3

--- PART/Source/CprBroker/DPR/Properties/AssemblyInfo.cs
+++ PART/Source/CprBroker/DPR/Properties/AssemblyInfo.cs
@@ -23,9 +23,6 @@
  *
  * Contributor(s):
  * Beemen Beshara
- * Niels Elgaard Larsen
- * Leif Lodahl
- * Steen Deth
  *
  * The code is currently governed by IT- og Telestyrelsen / Danish National
  * IT and Telecom Agency
@@ -78,5 +75,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.2.1")]
-[assembly: AssemblyFileVersion("2.2.2.1")]
+[assembly: AssemblyVersion("2.2.3.0")]
+[assembly: AssemblyFileVersion("2.2.3.0")]
