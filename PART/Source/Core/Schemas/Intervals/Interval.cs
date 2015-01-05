620b923ff4fb07ca5f8242a814c871944527a4de
beemen <beemen@magenta-aps.dk>
2015-01-05 11:26:55 +0100

    Removed TODO comment

--- PART/Source/Core/Schemas/Intervals/Interval.cs
+++ PART/Source/Core/Schemas/Intervals/Interval.cs
@@ -74,7 +74,7 @@ namespace CprBroker.Schemas.Part
             }
         }
 
-        // TODO: Remove this method if only used in tests
+        // Not used, but kept for tests to avoid creating an extra base class for stubs
         public static Interval[] CreateFromData(params ITimedType[] dataObjects)
         {
             return CreateFromData<Interval>(dataObjects.AsQueryable());
