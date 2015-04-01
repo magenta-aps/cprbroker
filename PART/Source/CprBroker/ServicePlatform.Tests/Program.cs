a0979ad0c49ea4c94353d56400c30089371c7da1
Beemen Beshara <beemen@magenta-aps.dk>
2015-04-01 10:01:42 +0200

    Included Test code

---
+++ PART/Source/CprBroker/ServicePlatform.Tests/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CprBroker.Schemas;
+using CprBroker.Schemas.Part;
+namespace CprBroker.Tests.ServicePlatform
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CprBroker.Engine.BrokerContext.Initialize(CprBroker.Utilities.Constants.BaseApplicationToken.ToString(), "");
+            
+            var prov = ServicePlatformDataProviderFactory.Create();
+
+            var inp = new SoegInputType1()
+            {
+                SoegObjekt = new SoegObjektType()
+                {
+                    SoegAttributListe = new SoegAttributListeType()
+                    {
+                        SoegRegisterOplysning = new RegisterOplysningType[]{
+                            new RegisterOplysningType(){
+                                Item = new CprBorgerType(){
+                                    FolkeregisterAdresse = new AdresseType(){
+                                        Item = new DanskAdresseType(){
+                                            AddressComplete = new AddressCompleteType(){
+                                                AddressPostal = new AddressPostalType(){
+                                                    StreetName = "Industrivej",
+                                                    StreetBuildingIdentifier = "1",
+                                                    PostCodeIdentifier = "2600"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var ret = prov.SearchList(inp);
+            //var ret2 = prov.PutSubscription(new PersonIdentifier() { CprNumber = "0123456789" });
+            CprBroker.Schemas.QualityLevel? ql;
+            foreach(var pnr in CprBroker.Tests.CPRDirect.Utilities.PNRs)
+            {
+                var ret3 = prov.Read(new PersonIdentifier() { CprNumber = pnr, UUID = Guid.NewGuid() }, null, p => Guid.NewGuid(), out ql);
+            }
+        }
+    }
+}
---
+++ PART/Source/CprBroker/ServicePlatform.Tests/ServicePlatformDataProviderFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using CprBroker.Providers.ServicePlatform;
+
+namespace CprBroker.Tests.ServicePlatform
+{
+    public class ServicePlatformDataProviderFactory
+    {
+        public static ServicePlatformDataProvider Create()
+        {
+            var lines = File.ReadAllLines(@"..\..\demo.dat");
+            var dic = new Dictionary<string, string>();
+            foreach (var l in lines)
+            {
+                var d = l.Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                if (d.Count() > 0)
+                {
+                    var key = d.First().TrimStart().TrimEnd();
+                    var val = d.Count() > 1 ? d[1].TrimStart().TrimEnd() : "";
+                    dic[key.ToLower()] = val;
+                }
+            }
+
+            var prov = new CprBroker.Providers.ServicePlatform.ServicePlatformDataProvider()
+            {
+                ConfigurationProperties = new Dictionary<string, string>(),
+                Url = "https://exttest.serviceplatformen.dk",
+                UserSystemUUID = dic["UserSystemUUID".ToLower()],
+                ServiceAgreementUuid = dic["ServiceAgreementUuid".ToLower()],
+                UserUUID = dic["UserUUID".ToLower()],
+                CertificateSerialNumber = dic["CertificateSerialNumber".ToLower()]
+            };
+
+            return prov;
+        }
+    }
+}
