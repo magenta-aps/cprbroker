765b4d22d0ac71aa76fed560161cecc778e182c0
Beemen Beshara <beemen@magenta-aps.dk>
2015-09-07 13:01:09 +0200

    Included foundation for lookup for ChurchAdministrationDistrict

---
+++ PART/Source/CprBroker/DPR/Data/ChurchAdministrationDistrict.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CprBroker.Providers.DPR
+{
+    public partial class ChurchAdministrationDistrict : IHouseLookup
+    {
+        public static decimal GetAuthorityCode(string connectionString, decimal municipalityCode, decimal streetCode, string houseNumber)
+        {
+            return HouseLookupHelper<ChurchAdministrationDistrict>
+                .GetPostValue<decimal>(connectionString, municipalityCode, streetCode, houseNumber, dc => dc.ChurchAdministrationDistricts, o => o.KOMKOD);
+        }
+    }
+}
