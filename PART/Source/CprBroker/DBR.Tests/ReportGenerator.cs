2541c4614b12bc0fcb0a95caacf6a5e1ae210782
Beemen Beshara <beemen@magenta-aps.dk>
2015-09-09 14:10:23 +0200

    Enhanced ReportGenerator

--- PART/Source/CprBroker/DBR.Tests/ReportGenerator.cs
+++ PART/Source/CprBroker/DBR.Tests/ReportGenerator.cs
@@ -21,7 +21,7 @@ namespace CprBroker.Tests.DBR.Comparison
             {
                 var dprType = t.BaseType.GetGenericArguments().FirstOrDefault();
                 var cmpObj = Reflection.CreateInstance(t);
-                sb.Append(String.Format("Type <{0}>, table <{1}>\r\n", dprType.Name, DataLinq.GetTableName(dprType)));
+                sb.Append(String.Format("* Type <{0}>, table <{1}>\r\n", dprType.Name, DataLinq.GetTableName(dprType)));
                 var props = DataLinq.GetColumnProperties(dprType);
                 var propNames = t.InvokeMember("ExcludedProperties", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.GetProperty, null, cmpObj, null) as string[];
                 if (propNames.Length > 0)
@@ -29,12 +29,12 @@ namespace CprBroker.Tests.DBR.Comparison
                     foreach (var propName in propNames)
                     {
                         var prop = props.Where(p => p.Name.Equals(propName)).Single();
-                        sb.Append(string.Format("    Property <{0}>, Column <{1}>\r\n", prop.Name, DataLinq.GetColumnName(prop)));
+                        sb.Append(string.Format("** Property <{0}>, Column <{1}>\r\n", prop.Name, DataLinq.GetColumnName(prop)));
                     }
                 }
                 else
                 {
-                    sb.Append("    (None)\r\n");
+                    sb.Append("** (None)\r\n");
                 }
             }
             return sb.ToString();
