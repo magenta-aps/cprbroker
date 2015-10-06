96a021a3952dbdf74ae17a6c5e8b60ede9feba0c
Beemen Beshara <beemen@magenta-aps.dk>
2015-10-06 10:19:35 +0200

    Added missing file

---
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/DataChangeEvent.sql
@@ -0,0 +1,18 @@
+﻿/****** Object:  Table [dbo].[DataChangeEvent]    Script Date: 11/21/2013 10:16:51 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[DataChangeEvent]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[DataChangeEvent](
+	[DataChangeEventId] [uniqueidentifier] NOT NULL
+		DEFAULT NEWID()
+		CONSTRAINT [PK_DataChangeEvent] PRIMARY KEY CLUSTERED (	[DataChangeEventId] ASC),
+	[PersonUuid] [uniqueidentifier] NOT NULL,
+	[PersonRegistrationId] [uniqueidentifier] NOT NULL,
+	[ReceivedDate] [datetime] NOT NULL,
+) ON [PRIMARY]
+END
+
+GO
