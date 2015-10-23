2e1deaf5074d301e7052a98dae1dbe785e9af4b3
Beemen Beshara <beemen@magenta-aps.dk>
2015-10-23 10:35:08 +0200

    Isolated relations test in service platform

--- PART/Source/CprBroker/ServicePlatform.Tests/FamilyPlusResponseTests.cs
+++ PART/Source/CprBroker/ServicePlatform.Tests/FamilyPlusResponseTests.cs
@@ -142,5 +142,140 @@ namespace CprBroker.Tests.ServicePlatform
                 Assert.Greater(count, 0);
             }
         }
+
+        [TestFixture]
+        public class ToRelationListeTypeTests : FamilyPlusResponseTestBase
+        {
+            public RelationListeType CallMethod(string pnr)
+            {
+                var cache = UuidCacheFactory.Create();
+
+                var familyPlus = new FamilyPlusResponse(GetResponse(pnr, ServiceInfo.FamilyPlus_Local.Name));
+
+                return familyPlus.ToRelationListeType(cpr => cache.GetUuid(cpr));
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveFather()
+            {
+                var ret = PNRs
+                    .Select(pnr => CallMethod(pnr))
+                    .Where(reg => reg.Fader != null
+                        && reg.Fader.FirstOrDefault() != null
+                        && !string.IsNullOrEmpty(reg.Fader.First().ReferenceID.Item));
+
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveMother()
+            {
+                var ret = PNRs
+                    .Select(pnr => CallMethod(pnr))
+                    .Where(reg => reg.Moder != null
+                        && reg.Moder.FirstOrDefault() != null
+                        && !string.IsNullOrEmpty(reg.Moder.First().ReferenceID.Item));
+
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveChildren()
+            {
+                var ret = PNRs
+                    .Select(pnr => CallMethod(pnr))
+                    .Where(reg => reg.Boern != null
+                        && reg.Boern.FirstOrDefault() != null
+                        && !string.IsNullOrEmpty(reg.Boern.First().ReferenceID.Item));
+
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            [Test]
+            [Ignore("Disempowerment not available in free services")]
+            public void ToRegistreringType1_SomeHaveGuardian()
+            {
+                var ret = PNRs
+                    .Select(pnr => CallMethod(pnr))
+                    .Where(reg => reg.RetligHandleevneVaergemaalsindehaver != null
+                        && reg.RetligHandleevneVaergemaalsindehaver.FirstOrDefault() != null
+                        && !string.IsNullOrEmpty(reg.RetligHandleevneVaergemaalsindehaver.First().ReferenceID.Item));
+
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveSpouse()
+            {
+                var ret = PNRs
+                    .Select(pnr => CallMethod(pnr))
+                    .Where(reg => reg.Aegtefaelle != null
+                        && reg.Aegtefaelle.FirstOrDefault() != null
+                        && !string.IsNullOrEmpty(reg.Aegtefaelle.First().ReferenceID.Item));
+
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveParentalAuthority()
+            {
+                var ret = PNRs
+                    .Select(pnr => CallMethod(pnr))
+                    .Where(reg => reg.Foraeldremyndighedsindehaver != null
+                        && reg.Foraeldremyndighedsindehaver.FirstOrDefault() != null
+                        && !string.IsNullOrEmpty(reg.Foraeldremyndighedsindehaver.First().ReferenceID.Item));
+
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            string[] GetParentUuids(RelationListeType reg)
+            {
+                var parentUuids = new List<string>();
+                if (reg.Fader.FirstOrDefault() != null)
+                    parentUuids.Add(reg.Fader.First().ReferenceID.Item);
+                if (reg.Moder.FirstOrDefault() != null)
+                    parentUuids.Add(reg.Moder.First().ReferenceID.Item);
+                return parentUuids.ToArray();
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveParentalAuthorityFromParents()
+            {
+                var ret = PNRs
+                    .Select(pnr => new { PNR = pnr, Rel = CallMethod(pnr) })
+                    .Where(
+                        reg => reg.Rel.Foraeldremyndighedsindehaver != null
+                        && reg.Rel.Foraeldremyndighedsindehaver.FirstOrDefault() != null
+                        && reg.Rel.Foraeldremyndighedsindehaver.Where(
+                            p =>
+                                GetParentUuids(reg.Rel).Contains(p.ReferenceID.Item)
+                          ).Count() > 0
+                        );
+                Console.WriteLine("Found: " + ret.Count());
+                foreach (var reg in ret)
+                    Console.WriteLine(reg.PNR);
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+            [Test]
+            public void ToRegistreringType1_SomeHaveParentalAuthorityFromNonParents()
+            {
+                var ret = PNRs
+                    .Select(pnr => new { PNR = pnr, Rel = CallMethod(pnr) })
+                    .Where(
+                        reg => reg.Rel.Foraeldremyndighedsindehaver != null
+                        && reg.Rel.Foraeldremyndighedsindehaver.FirstOrDefault() != null
+                        && reg.Rel.Foraeldremyndighedsindehaver.Where(
+                            p =>
+                                !GetParentUuids(reg.Rel).Contains(p.ReferenceID.Item)
+                          ).Count() > 0
+                        );
+                Console.WriteLine("Found: " + ret.Count());
+                foreach (var reg in ret)
+                    Console.WriteLine(reg.PNR);
+                Assert.NotNull(ret.FirstOrDefault());
+            }
+
+        }
     }
 }
