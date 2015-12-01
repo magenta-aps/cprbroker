f134801f51151749a462b0a5a8422d67f49ab3ed
Beemen Beshara <beemen@magenta-aps.dk>
2015-12-01 14:37:28 +0100

    controller actions for putting and removing subscriptions

--- PART/Source/CprBroker/Web/Controllers/SubscriptionDataProviderController.cs
+++ PART/Source/CprBroker/Web/Controllers/SubscriptionDataProviderController.cs
@@ -8,15 +8,70 @@ using System.Data.Linq;
 
 namespace CprBroker.Web.Controllers
 {
+    [RoutePrefix("mvc/pages/dataproviders")]
     public class SubscriptionDataProviderController : Controller
     {
-        // GET: SubscriptionDataProvider
+        [Route("{dataProviderId:Guid}")]
         public ActionResult ViewDetails(Guid dataProviderId)
         {
+            CprBroker.Engine.BrokerContext.Initialize(
+                CprBroker.Utilities.Constants.BaseApplicationToken.ToString(),
+                this.HttpContext.User.Identity.Name);
+
             var factory = new DataProviderFactory();
             var dataProvider = factory.GetDataProvider<ISubscriptionManagerDataProvider>(dataProviderId);
 
-            return View("DataProviderView", dataProvider);
+            return View("DataProviderView", new Tuple<ISubscriptionManagerDataProvider, Guid>(dataProvider, dataProviderId));
+        }
+
+        [Route("{dataProviderId:Guid}/putsubscription/{field}/{value}")]
+        public ActionResult PutSubscription(Guid dataProviderId, string field, string value)
+        {
+            CprBroker.Engine.BrokerContext.Initialize(
+                CprBroker.Utilities.Constants.BaseApplicationToken.ToString(),
+                this.HttpContext.User.Identity.Name);
+
+            var factory = new DataProviderFactory();
+            var dataProvider = factory.GetDataProvider<ISubscriptionManagerDataProvider>(dataProviderId);
+
+            try
+            {
+                var ret = dataProvider.PutSubscription(field, value);
+                if (ret)
+                    return new HttpStatusCodeResult(200);
+                else
+                    return new HttpStatusCodeResult(500, "PutSubscription failed");
+            }
+            catch (Exception ex)
+            {
+                CprBroker.Engine.Local.Admin.LogException(ex);
+                return new HttpStatusCodeResult(500, ex.ToString());
+            }
+        }
+
+        [Route("{dataProviderId:Guid}/removesubscription/{field}/{value}")]
+        public ActionResult RemoveSubscription(Guid dataProviderId, string field, string value)
+        {
+            CprBroker.Engine.BrokerContext.Initialize(
+                CprBroker.Utilities.Constants.BaseApplicationToken.ToString(),
+                this.HttpContext.User.Identity.Name);
+
+            var factory = new DataProviderFactory();
+            var dataProvider = factory.GetDataProvider<ISubscriptionManagerDataProvider>(dataProviderId);
+
+            try
+            {
+                var ret = dataProvider.RemoveSubscription(field, value);
+                if (ret)
+                    return new HttpStatusCodeResult(200);
+                else
+                    return new HttpStatusCodeResult(500, "RemoveSubscription failed");
+            }
+            catch (Exception ex)
+            {
+                CprBroker.Engine.Local.Admin.LogException(ex);
+                return new HttpStatusCodeResult(500, ex.ToString());
+            }
         }
     }
 }
