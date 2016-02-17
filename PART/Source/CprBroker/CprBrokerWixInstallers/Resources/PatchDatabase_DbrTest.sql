cb84de3b9da5f16d4f5b61bb43a82445514ccba5
beemen <beshara_beemen@hotmail.com>
2016-02-17 10:48:24 +0100

    Added missing filr

---
+++ PART/Source/CprBroker/CprBrokerWixInstallers/Resources/PatchDatabase_DbrTest.sql
@@ -0,0 +1,7 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.indexes si WHERE si.name = N'IX_QueueItem_QueueId')
+	CREATE NONCLUSTERED INDEX [IX_QueueItem_QueueId] ON [dbo].[QueueItem] 
+	(
+		[QueueId] ASC
+	)
+	INCLUDE ( [AttemptCount],[SemaphoreId]) 
+GO
