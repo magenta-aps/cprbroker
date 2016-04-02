af0f4234509e76e99bc452166dc38b935b18bdd8
beemen <beshara_beemen@hotmail.com>
2016-04-02 13:21:42 +0200

    Isolation of reduced records code

--- PART/Source/CprBroker/CPRDirect.Tests/Partials/PartialRecordTests.cs
+++ PART/Source/CprBroker/CPRDirect.Tests/Partials/PartialRecordTests.cs
@@ -5,55 +5,33 @@ using System.Text;
 using NUnit.Framework;
 using CprBroker.Providers.CPRDirect;
 using CprBroker.Schemas.Part;
+using CprBroker.Engine.Part;
+using CprBroker.Utilities;
 
 namespace CprBroker.Tests.CPRDirect.Partials
 {
     [TestFixture]
     class PartialRecordTests
     {
+        UuidCache UuidCache = new UuidCache()
+        {
+            GetUuidMethod = (pnr) => Guid.NewGuid(),
+            GetUuidArrayMethod = (pnrs) => pnrs.Select(pnr => Guid.NewGuid() as Guid?).ToArray(),
+        };
+
         Guid CprToUuid(string pnr)
         {
-            return Guid.NewGuid();
+            return UuidCache.GetUuid(pnr);
         }
 
         [Test]
-        public void ToRegistreringType1(
+        public void ToRegistreringType1_Reduced_HasNameAddressPnr(
             [ValueSource(typeof(Utilities), "PNRs")]string pnr)
         {
-            System.Diagnostics.Debugger.Launch();
             var all = IndividualResponseType.ParseBatch(Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE);
             var pers = all.Where(p => p.PersonInformation.PNR == pnr).First();
 
-            //Todo: Beemen should review these.
-            //Do the data correspond to the record numbers?
-            //I'm mostly in doubt regarding the ones marked with '?'.
-            pers.CurrentDepartureData = null;  //005 ?
-            pers.CurrentDisappearanceInformation = null;  //007
-            pers.BirthRegistrationInformation = null;  //009
-            pers.CurrentCitizenship = null;  //010
-            pers.ChurchInformation = null;  //011
-            pers.CurrentCivilStatus = null;  //012
-            pers.CurrentSeparation = null;  //013
-            pers.MunicipalConditions = null;  //018
-            pers.Notes = null;  //019
-            pers.ElectionInformation = null;  //020
-            pers.RelocationOrder = null;  //021
-
-            pers.HistoricalPNR.Clear();  //022 ?
-            pers.HistoricalAddress.Clear();  //023
-            pers.HistoricalDeparture.Clear();  //024 ?
-            pers.HistoricalDisappearance.Clear();  //025
-            pers.HistoricalName.Clear();  //026
-            pers.HistoricalCitizenship.Clear();  //027
-            pers.HistoricalChurchInformation.Clear();  //028
-            pers.HistoricalCivilStatus.Clear();  //029
-            pers.HistoricalSeparation.Clear();  //030
-
-            pers.Events = null;  //099 ?
-            pers.ErrorRecord = null;  //910 ?
-            pers.SubscriptionDeletionReceipt = null;  //997 ?
-
-            //What about 999?
+            ClearOptionalRecords(pers);
 
             var registration = pers.ToRegistreringType1(CprToUuid);
 
@@ -70,20 +48,12 @@ namespace CprBroker.Tests.CPRDirect.Partials
                 if (pers.PersonInformation.Status != 70 && pers.PersonInformation.Status != 80 && pers.PersonInformation.Status != 90) // Not disappeared, emigrated or dead
                     Assert.IsNotNull(borgerType.FolkeregisterAdresse);
             }
-            Assert.IsNotNull(registration.AttributListe.Egenskab[0].NavnStruktur);
+            Assert.IsNotNull(registration.AttributListe.Egenskab[0].NavnStruktur, "Name not found");
+            Assert.IsNotNull(registration.TilstandListe.LivStatus, "Life status not found");
         }
 
-        [Test]
-        public void ToRegistreringType1_CompareBeforeAndAfterClear(
-            [ValueSource(typeof(Utilities), "PNRs")]string pnr)
+        private static void ClearOptionalRecords(IndividualResponseType pers)
         {
-            System.Diagnostics.Debugger.Launch();
-            var all = IndividualResponseType.ParseBatch(Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE);
-            var pers = all.Where(p => p.PersonInformation.PNR == pnr).First();
-            var registrationBefore = pers.ToRegistreringType1(CprToUuid);
-            registrationBefore.OrderByStartDate(false);
-            var borgerTypeBefore = (CprBorgerType)registrationBefore.AttributListe.RegisterOplysning[0].Item;
-
             //Todo: Beemen should review these.
             //Do the data correspond to the record numbers?
             //I'm mostly in doubt regarding the ones marked with '?'.
@@ -109,11 +79,22 @@ namespace CprBroker.Tests.CPRDirect.Partials
             pers.HistoricalCivilStatus.Clear();  //029
             pers.HistoricalSeparation.Clear();  //030
 
-            pers.Events = null;  //099 ?
+            pers.Events.Clear();  //099 ?
             pers.ErrorRecord = null;  //910 ?
-            pers.SubscriptionDeletionReceipt = null;  //997 ?
+            pers.SubscriptionDeletionReceipt.Clear();  //997 ?
+        }
 
-            //What about 999?
+        [Test]
+        public void ToRegistreringType1_Reduced_CompareWithFull(
+            [ValueSource(typeof(Utilities), "PNRs")]string pnr)
+        {
+            var all = IndividualResponseType.ParseBatch(Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE);
+            var pers = all.Where(p => p.PersonInformation.PNR == pnr).First();
+            var registrationBefore = pers.ToRegistreringType1(CprToUuid);
+            registrationBefore.OrderByStartDate(false);
+            var borgerTypeBefore = (CprBorgerType)registrationBefore.AttributListe.RegisterOplysning[0].Item;
+
+            ClearOptionalRecords(pers);
 
             var registration = pers.ToRegistreringType1(CprToUuid);
 
@@ -138,44 +119,24 @@ namespace CprBroker.Tests.CPRDirect.Partials
                     CprBroker.Utilities.Strings.SerializeObject(registrationBefore.AttributListe.Egenskab[0].NavnStruktur),
                     CprBroker.Utilities.Strings.SerializeObject(registration.AttributListe.Egenskab[0].NavnStruktur)
                 );
-
             }
+
+            StringAssert.AreEqualIgnoringCase(
+                    Strings.SerializeObject(registrationBefore.TilstandListe.LivStatus),
+                    Strings.SerializeObject(registration.TilstandListe.LivStatus)
+                );
         }
 
         [Test]
         public void ToFiltreretOejebliksbilledeType(
             [ValueSource(typeof(Utilities), "PNRs")]string pnr)
         {
-            System.Diagnostics.Debugger.Launch();
             var historyResponse = new IndividualHistoryResponseType(
                 new Schemas.PersonIdentifier() { CprNumber = pnr, UUID = Guid.NewGuid() },
                 (new IndividualResponseType[] { CprBroker.Tests.CPRDirect.Persons.Person.GetPerson(pnr) }).AsQueryable());
             var pers = historyResponse.IndividualResponseObjects.First();
-            pers.CurrentDepartureData = null;  //005 ?
-            pers.CurrentDisappearanceInformation = null;  //007
-            pers.BirthRegistrationInformation = null;  //009
-            pers.CurrentCitizenship = null;  //010
-            pers.ChurchInformation = null;  //011
-            pers.CurrentCivilStatus = null;  //012
-            pers.CurrentSeparation = null;  //013
-            pers.MunicipalConditions = null;  //018
-            pers.Notes = null;  //019
-            pers.ElectionInformation = null;  //020
-            pers.RelocationOrder = null;  //021
 
-            pers.HistoricalPNR.Clear();  //022 ?
-            pers.HistoricalAddress.Clear();  //023
-            pers.HistoricalDeparture.Clear();  //024 ?
-            pers.HistoricalDisappearance.Clear();  //025
-            pers.HistoricalName.Clear();  //026
-            pers.HistoricalCitizenship.Clear();  //027
-            pers.HistoricalChurchInformation.Clear();  //028
-            pers.HistoricalCivilStatus.Clear();  //029
-            pers.HistoricalSeparation.Clear();  //030
-
-            pers.Events = null;  //099 ?
-            pers.ErrorRecord = null;  //910 ?
-            pers.SubscriptionDeletionReceipt = null;  //997 ?
+            ClearOptionalRecords(pers);
 
             //What about 999?
             var registration = historyResponse.ToFiltreretOejebliksbilledeType(CprToUuid);
@@ -200,7 +161,6 @@ namespace CprBroker.Tests.CPRDirect.Partials
         public void ToFiltreretOejebliksbilledeType_CompareBeforeAndAfterClear(
             [ValueSource(typeof(Utilities), "PNRs")]string pnr)
         {
-            System.Diagnostics.Debugger.Launch();
             var historyResponse = new IndividualHistoryResponseType(
                 new Schemas.PersonIdentifier() { CprNumber = pnr, UUID = Guid.NewGuid() },
                 (new IndividualResponseType[] { CprBroker.Tests.CPRDirect.Persons.Person.GetPerson(pnr) }).AsQueryable());
@@ -208,31 +168,8 @@ namespace CprBroker.Tests.CPRDirect.Partials
                 new Schemas.PersonIdentifier() { CprNumber = pnr, UUID = Guid.NewGuid() },
                 (new IndividualResponseType[] { CprBroker.Tests.CPRDirect.Persons.Person.GetPerson(pnr) }).AsQueryable());
             var pers = historyResponse.IndividualResponseObjects.First();
-            pers.CurrentDepartureData = null;  //005 ?
-            pers.CurrentDisappearanceInformation = null;  //007
-            pers.BirthRegistrationInformation = null;  //009
-            pers.CurrentCitizenship = null;  //010
-            pers.ChurchInformation = null;  //011
-            pers.CurrentCivilStatus = null;  //012
-            pers.CurrentSeparation = null;  //013
-            pers.MunicipalConditions = null;  //018
-            pers.Notes = null;  //019
-            pers.ElectionInformation = null;  //020
-            pers.RelocationOrder = null;  //021
 
-            pers.HistoricalPNR.Clear();  //022 ?
-            pers.HistoricalAddress.Clear();  //023
-            pers.HistoricalDeparture.Clear();  //024 ?
-            pers.HistoricalDisappearance.Clear();  //025
-            pers.HistoricalName.Clear();  //026
-            pers.HistoricalCitizenship.Clear();  //027
-            pers.HistoricalChurchInformation.Clear();  //028
-            pers.HistoricalCivilStatus.Clear();  //029
-            pers.HistoricalSeparation.Clear();  //030
-
-            pers.Events = null;  //099 ?
-            pers.ErrorRecord = null;  //910 ?
-            pers.SubscriptionDeletionReceipt = null;  //997 ?
+            ClearOptionalRecords(pers);
 
             //What about 999?
             var registration = historyResponse.ToFiltreretOejebliksbilledeType(CprToUuid);
@@ -245,8 +182,9 @@ namespace CprBroker.Tests.CPRDirect.Partials
 
             Assert.AreNotSame(borgerType, borgerTypeBefore);
             Assert.AreNotSame(registration, registrationBefore);
-            if (pers.PersonInformation.Status == 1 || pers.PersonInformation.Status == 3)
+            if (pers.PersonInformation.Status <= 7)
             {
+                Console.WriteLine("{0} {1}", pnr, pers.PersonInformation.Status);
                 StringAssert.AreEqualIgnoringCase(
                     CprBroker.Utilities.Strings.SerializeObject(borgerTypeBefore.FolkeregisterAdresse.Item),
                     CprBroker.Utilities.Strings.SerializeObject(borgerType.FolkeregisterAdresse.Item)
@@ -259,8 +197,12 @@ namespace CprBroker.Tests.CPRDirect.Partials
                     CprBroker.Utilities.Strings.SerializeObject(registrationBefore.AttributListe.Egenskab.Last().NavnStruktur),
                     CprBroker.Utilities.Strings.SerializeObject(registration.AttributListe.Egenskab.Last().NavnStruktur)
                 );
-
             }
+
+            StringAssert.AreEqualIgnoringCase(
+                Strings.SerializeObject(registrationBefore.TilstandListe.LivStatus),
+                Strings.SerializeObject(registration.TilstandListe.LivStatus)
+            );
         }
     }
 }
