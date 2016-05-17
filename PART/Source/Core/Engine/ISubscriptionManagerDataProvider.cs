c1392715d1d61238bdc289757dcfd7e314c68fd4
beemen <beshara_beemen@hotmail.com>
2016-05-17 11:41:19 +0200

    Extension method for mass subscribe/unsubscribe

--- PART/Source/Core/Engine/ISubscriptionManagerDataProvider.cs
+++ PART/Source/Core/Engine/ISubscriptionManagerDataProvider.cs
@@ -18,4 +18,29 @@ namespace CprBroker.Engine
 
         Dictionary<string, string> EnumerateField(string field);
     }
+
+    public static class ISubscriptionManagerDataProviderExtensions
+    {
+        public static void MergeSubscriptions(this ISubscriptionManagerDataProvider prov, string field, string[] values, bool replace = false)
+        {
+            var existing = prov.GetSubscriptions(field);
+
+            // Only add a subscription if not already there
+            string[] toAdd = values.Except(existing).ToArray();
+
+            string[] toRemove = replace ?
+                existing.Except(values).ToArray() // Unsubscribe to keys that are no longer needed
+                : new string[] { }; // Do not remove any subscriptions
+
+            foreach (var value in toAdd)
+            {
+                prov.PutSubscription(field, value);
+            }
+
+            foreach (var value in toRemove)
+            {
+                prov.RemoveSubscription(field, value);
+            }
+        }
+    }
 }
