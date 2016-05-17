c933911bc9dfe49f936697422cd8abb88688abcf
beemen <beshara_beemen@hotmail.com>
2016-05-17 15:03:38 +0200

    Code refactoring

--- PART/Source/Core/Engine/ISubscriptionManagerDataProvider.cs
+++ PART/Source/Core/Engine/ISubscriptionManagerDataProvider.cs
@@ -19,10 +19,31 @@ namespace CprBroker.Engine
         Dictionary<string, string> EnumerateField(string field);
     }
 
+    public class SubcriptionMergeResult
+    {
+        public string Field { get; private set; }
+
+        public List<string> Added = new List<string>();
+        public List<string> AddFailed = new List<string>();
+        public List<string> Removed = new List<string>();
+        public List<string> RemoveFailed = new List<string>();
+        public List<string> AlreadyExisting = new List<string>();
+        public List<string> Kept = new List<string>();
+
+        public SubcriptionMergeResult(string field)
+        {
+            this.Field = field;
+        }
+
+    }
     public static class ISubscriptionManagerDataProviderExtensions
     {
-        public static void MergeSubscriptions(this ISubscriptionManagerDataProvider prov, string field, string[] values, bool replace = false)
+        public static SubcriptionMergeResult MergeSubscriptions(this ISubscriptionManagerDataProvider prov, string field, string[] values, bool replace = false)
         {
+            values = values.Distinct().ToArray();
+
+            var ret = new SubcriptionMergeResult(field);
+
             var existing = prov.GetSubscriptions(field);
 
             // Only add a subscription if not already there
@@ -32,15 +53,46 @@ namespace CprBroker.Engine
                 existing.Except(values).ToArray() // Unsubscribe to keys that are no longer needed
                 : new string[] { }; // Do not remove any subscriptions
 
+            ret.AlreadyExisting = values.Except(toAdd).ToList();
+            ret.Kept = existing.Except(toRemove).ToList();
+
             foreach (var value in toAdd)
             {
-                prov.PutSubscription(field, value);
+                var res = false;
+                try
+                {
+                    res = prov.PutSubscription(field, value);
+                }
+                catch (Exception ex)
+                {
+                    CprBroker.Engine.Local.Admin.LogException(ex);
+                }
+
+                if (res)
+                    ret.Added.Add(value);
+                else
+                    ret.AddFailed.Add(value);
             }
 
             foreach (var value in toRemove)
             {
-                prov.RemoveSubscription(field, value);
+                var res = false;
+                try
+                {
+                    res = prov.RemoveSubscription(field, value);
+                }
+                catch (Exception ex)
+                {
+                    CprBroker.Engine.Local.Admin.LogException(ex);
+                }
+
+                if (res)
+                    ret.Removed.Add(value);
+                else
+                    ret.RemoveFailed.Add(value);
             }
+
+            return ret;
         }
     }
 }
