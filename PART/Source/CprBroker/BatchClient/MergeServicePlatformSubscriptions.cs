c0054605a93c3a3883ffc4fac1c315af7b7fbf64
beemen <beshara_beemen@hotmail.com>
2016-05-18 15:39:03 +0200

    Isolated subscription merge in a batch client task

--- PART/Source/CprBroker/BatchClient/MergeServicePlatformSubscriptions.cs
+++ PART/Source/CprBroker/BatchClient/MergeServicePlatformSubscriptions.cs
@@ -2,8 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CprBroker.Providers.ServicePlatform;
+using CprBroker.Engine;
+using CprBroker.Utilities.ConsoleApps;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace BatchClient
 {
-    
+    public class MergeServicePlatformSubscriptions : ConsoleEnvironment
+    {
+        ServicePlatformDataProvider prov;
+        Dictionary<string, string[]> existing;
+        string[] pnrFiles;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            if (!string.IsNullOrEmpty(this.BrokerConnectionString))
+                Utilities.UpdateConnectionString(this.BrokerConnectionString);
+            var token = string.IsNullOrEmpty(this.ApplicationToken) ? CprBroker.Utilities.Constants.BaseApplicationToken.ToString() : this.ApplicationToken;
+
+            CprBroker.Engine.BrokerContext.Initialize(token, "");
+
+            var sourceFiles = this.SourceFile.Split(",;".ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            prov = CprBroker.Tests.ServicePlatform.ServicePlatformDataProviderFactory.Create(sourceFiles.First());
+            pnrFiles = sourceFiles.Skip(1).ToArray();
+
+            existing = prov.SubscriptionFields
+                .Select(f => new { f, values = prov.GetSubscriptions(f) })
+                .ToDictionary(f => f.f, f => f.values);
+        }
+
+        public override string[] LoadCprNumbers()
+        {
+            return pnrFiles
+                .SelectMany(f => File.ReadAllLines(f))
+                .Where(v => !string.IsNullOrEmpty(v))
+                .Select(v => v.Length <= 3 ? v.PadLeft(4,'0') :
+                    Regex.IsMatch(v, @"\A\d{9,10}\Z") ? v.PadLeft(10, '0') :
+                    v
+                ).ToArray();
+        }
+
+        public override void ProcessPerson(string pnr)
+        {
+            var field = pnr.Length == 4 ? Constants.SubscriptionFields.MunicipalityCode :
+                pnr.Length == 10 ? Constants.SubscriptionFields.PNR :
+                Constants.SubscriptionFields.MunicipalityCode;
+
+            if (existing[field].Contains(pnr))
+            {
+                Log("Subscription exists, skipping");
+            }
+            else
+            {
+                Log("Subscriping");
+                ServicePlatformDataProvider.ReturnCodePNR retCode;
+                if (prov.PutSubscription(field, pnr, out retCode))
+                { }
+                else
+                {
+                    throw new Exception(string.Format("Failed, reason = {0}", retCode));
+                }
+            }
+        }
+    }
 }
