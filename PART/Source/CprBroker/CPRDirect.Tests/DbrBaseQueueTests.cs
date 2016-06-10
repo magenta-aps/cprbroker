05586090969c0827002498c93067fbb84baaece4
beemen <beshara_beemen@hotmail.com>
2016-06-10 13:28:19 +0200

    Moved DbrBaseQueueTests to CPRDirect.Tests

---
+++ PART/Source/CprBroker/CPRDirect.Tests/DbrBaseQueueTests.cs
@@ -0,0 +1,47 @@
+﻿using CprBroker.Providers.CPRDirect;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CprBroker.Tests.CPRDirect
+{
+    [TestFixture]
+    public class DbrBaseQueueTests : CprBroker.Tests.PartInterface.TestBase
+    {
+        public class DbrQueueStub : CprBroker.Engine.Queues.Queue<ExtractQueueItem>
+        {
+            public override ExtractQueueItem[] Process(ExtractQueueItem[] items)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [SetUp]
+        public void InitApplication()
+        {
+            CprBroker.Engine.BrokerContext.Initialize(CprBroker.Utilities.Constants.BaseApplicationToken.ToString(), "Test user");
+        }
+
+        [Test]
+        public void RunOneBatch_GoesToDbrQueue()
+        {
+            // Init DB
+            CprBroker.Providers.CPRDirect.ExtractManager.ImportText(CprBroker.Tests.CPRDirect.Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE_FixedLength);
+            var stagingQueue = CprBroker.Engine.Queues.Queue.GetQueues<CprBroker.Providers.CPRDirect.ExtractStagingQueue>().Single();
+            var dbrBaseQueue = CprBroker.Engine.Queues.Queue.GetQueues<CprBroker.Providers.CPRDirect.DbrBaseQueue>().Single();
+
+            var values = new Dictionary<string, string>();
+            CprBroker.Engine.DataProviderConfigProperty.Templates.SetConnectionString("", values);
+            var dbrQueue = CprBroker.Engine.Queues.Queue.AddQueue<DbrQueueStub>(CprBroker.Providers.CPRDirect.DbrBaseQueue.TargetQueueTypeId, values, 100, 1);
+
+            // Now run
+            stagingQueue.RunOneBatch();
+            dbrBaseQueue.RunOneBatch();
+            var items = dbrQueue.GetNext(1);
+            Assert.IsNotEmpty(items);
+        }
+    }
+}
--- PART/Source/CprBroker/DBR.Tests/ImportAndQueueTests.cs
+++ PART/Source/CprBroker/DBR.Tests/ImportAndQueueTests.cs
@@ -9,30 +9,6 @@ namespace CprBroker.Tests.DBR
 {
     namespace QueueTests
     {
-        
-        [TestFixture]
-        public class DbrBaseQueueTests : DbrTestBase
-        {
-            [Test]
-            public void RunOneBatch_GoesToDbrQueue()
-            {
-                // Init DB
-                CprBroker.Providers.CPRDirect.ExtractManager.ImportText(CprBroker.Tests.CPRDirect.Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE_FixedLength);
-                var stagingQueue = CprBroker.Engine.Queues.Queue.GetQueues<CprBroker.Providers.CPRDirect.ExtractStagingQueue>().Single();
-                var dbrBaseQueue = CprBroker.Engine.Queues.Queue.GetQueues<CprBroker.Providers.CPRDirect.DbrBaseQueue>().Single();
-
-                var values = new Dictionary<string, string>();
-                CprBroker.Engine.DataProviderConfigProperty.Templates.SetConnectionString(DbrDatabase.ConnectionString, values);
-                var dbrQueue = CprBroker.Engine.Queues.Queue.AddQueue<DbrQueue>(CprBroker.Providers.CPRDirect.DbrBaseQueue.TargetQueueTypeId, values, 100, 1);
-
-                // Now run
-                stagingQueue.RunOneBatch();
-                dbrBaseQueue.RunOneBatch();
-                var items = dbrQueue.GetNext(1);
-                Assert.IsNotEmpty(items);
-            }
-        }
-
         [TestFixture]
         public class DbrQueueTests : DbrTestBase
         {
