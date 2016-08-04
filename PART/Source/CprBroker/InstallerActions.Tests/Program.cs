471a12269fe2eaac555b1116f87e7f21b62bdedc
beemen <beshara_beemen@hotmail.com>
2016-08-04 16:16:06 +0200

    Fix for last commit

--- PART/Source/CprBroker/InstallerActions.Tests/Program.cs
+++ PART/Source/CprBroker/InstallerActions.Tests/Program.cs
@@ -1,17 +1,16 @@
-﻿using CprBroker.Tests.CprBrokerCustomActionTests;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CprBroker.InstallerActions.Tests
+namespace CprBroker.Tests.InstallerActions
 {
     class Program
     {
         public static void Main()
         {
-            new PatchWebsite_2_2_7().PatchWebsite_2_2_7_Normal();
+            new CprBrokerCustomActionTests.PatchWebsite_2_2_7().PatchWebsite_2_2_7_Passes();
         }
     }
 }
