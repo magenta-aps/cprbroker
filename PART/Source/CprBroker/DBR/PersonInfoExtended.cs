1cc9efda17c937efb95d1132e4e68a23f130ab85
beemen <beshara_beemen@hotmail.com>
2016-10-04 16:29:32 +0200

    New method: PersonInfoExtended.Load(..)

--- PART/Source/CprBroker/DBR/PersonInfoExtended.cs
+++ PART/Source/CprBroker/DBR/PersonInfoExtended.cs
@@ -11,5 +11,16 @@ namespace CprBroker.DBR
     {
         public Person Person { get; set; }
         public Disappearance Disappearance { get; set; }
+
+        public PersonInfoExtended Load(DPRDataContext dataContext, decimal pnr)
+        {
+            var personTotal = dataContext.PersonTotals.SingleOrDefault(t => t.PNR == pnr);
+            var ret = personTotal.ToPersonInfo<PersonInfoExtended>();
+
+            ret.Person = dataContext.Persons.SingleOrDefault(p => p.PNR == pnr);
+            ret.Disappearance = dataContext.Disappearances.SingleOrDefault(d => d.PNR == pnr && d.CorrectionMarker == null);
+
+            return ret;
+        }
     }
 }
