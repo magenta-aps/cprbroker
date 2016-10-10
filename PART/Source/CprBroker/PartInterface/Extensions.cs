3786b91688a33aa0dbc86128de07312e1f2a2097
beemen <beshara_beemen@hotmail.com>
2016-10-10 15:06:11 +0200

    decimal.ToPnrDecimalString() has an optional parameter to allow adding a middle dash

--- PART/Source/CprBroker/PartInterface/Extensions.cs
+++ PART/Source/CprBroker/PartInterface/Extensions.cs
@@ -8,11 +8,13 @@ namespace CprBroker.Utilities
 {
     public static class Extensions
     {
-        public static string ToPnrDecimalString(this decimal val)
+        public static string ToPnrDecimalString(this decimal val, bool dash = false)
         {
             var ret = val.ToDecimalString();
             int cprNumberLength = 10;
             ret = new string('0', cprNumberLength - ret.Length) + ret;
+            if (dash)
+                ret = ret.Substring(0, 6) + "-" + ret.Substring(6, 4);
             return ret;
         }
     }
