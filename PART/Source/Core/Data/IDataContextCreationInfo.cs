48c2eba8ddd0d742f95c9674d32f6b1e7615becb
beemen <beshara_beemen@hotmail.com>
2016-10-21 10:57:00 +0200

    New interface: IDataContextCreationInfo

---
+++ PART/Source/Core/Data/IDataContextCreationInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CprBroker.Data
+{
+    public interface IDataContextCreationInfo
+    {
+        string DDL { get; }
+        KeyValuePair<string, string>[] Lookups { get; }
+        Action<SqlConnection> CustomInitializer { get; }
+    }
+}
