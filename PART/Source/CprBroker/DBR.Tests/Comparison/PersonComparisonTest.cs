7c3d665c4ccde70f555637ae18ab73a9a3abca5f
beemen <beshara_beemen@hotmail.com>
2016-10-27 11:04:07 +0200

    Isolated _PersonConversion class in a separate file

--- PART/Source/CprBroker/DBR.Tests/Comparison/PersonComparisonTest.cs
+++ PART/Source/CprBroker/DBR.Tests/Comparison/PersonComparisonTest.cs
@@ -118,39 +118,5 @@ namespace CprBroker.Tests.DBR.Comparison.Person
             return new DPRDataContext(connectionString);
         }
     }
-
-    [TestFixture]
-    public class _PersonConversion : PersonComparisonTest<object>
-    {
-        public void ConvertObject(string pnr)
-        {
-            using (var fakeDprDataContext = new DPRDataContext(Properties.Settings.Default.ImitatedDprConnectionString))
-            {
-                DatabaseLoadCache.Root.Reset(fakeDprDataContext);
-
-                CprConverter.DeletePersonRecords(pnr, fakeDprDataContext);
-                fakeDprDataContext.SubmitChanges();
-            }
-            using (var fakeDprDataContext = new DPRDataContext(Properties.Settings.Default.ImitatedDprConnectionString))
-            {
-                var person = ExtractManager.GetPerson(pnr);
-                CprConverter.AppendPerson(person, fakeDprDataContext);
-                fakeDprDataContext.SubmitChanges();
-            }
-            KeysHolder._ConvertedPersons[pnr] = true;
-        }
-
-        [Test]
-        [TestCaseSource("LoadKeys")]
-        public void T0_Convert(string key)
-        {
-            ConvertObject(key);
-        }
-
-        public override IQueryable<object> Get(DPRDataContext dataContext, string key)
-        {
-            return new object[] { }.AsQueryable();
-        }
-
-    }
+    
 }
