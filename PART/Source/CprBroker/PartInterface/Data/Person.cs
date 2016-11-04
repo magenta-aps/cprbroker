d1bc0b1df8a8b0db01915fc3fc0505d87da560fd
beemen <beshara_beemen@hotmail.com>
2016-11-04 13:43:32 +0100

    Untested code for deletion of Person table and its child tables)

---
+++ PART/Source/CprBroker/PartInterface/Data/Person.cs
@@ -0,0 +1,30 @@
+﻿using CprBroker.Data.Part;
+using CprBroker.Schemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CprBroker.Data.Part
+{
+    public partial class Person
+    {
+        public static void Delete(PersonIdentifier personIdentifier)
+        {
+            using (var dataContext = new PartDataContext())
+            {
+                var registrationsToDelete = dataContext.PersonRegistrations.Where(pr => pr.UUID == personIdentifier.UUID);
+                dataContext.PersonRegistrations.DeleteAllOnSubmit(registrationsToDelete);
+
+                var actorsToDelete = registrationsToDelete.Select(r => r.ActorRef);
+                dataContext.ActorRefs.DeleteAllOnSubmit(actorsToDelete);
+
+                var personsToDelete = dataContext.Persons.Where(p => p.UUID == personIdentifier.UUID);
+                dataContext.Persons.DeleteAllOnSubmit(personsToDelete);
+
+                dataContext.SubmitChanges();
+            }
+        }
+    }
+}
