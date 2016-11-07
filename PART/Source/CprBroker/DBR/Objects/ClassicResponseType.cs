b8c55c6136210086ab0955aa097a1b89e645f9a4
beemen <beshara_beemen@hotmail.com>
2016-11-07 14:42:55 +0100

    Test for removal of extract items

---
+++ PART/Source/CprBroker/DBR/Objects/ClassicResponseType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CprBroker.DBR
+{
+    public partial class ClassicResponseType
+    {
+        public override string ToString()
+        {
+            return base.ToString().TrimEnd();
+        }
+    }
+}
--- PART/Source/CprBroker/Tracking.Tests/TrackingDataProviderTests.RemovePerson.cs
+++ PART/Source/CprBroker/Tracking.Tests/TrackingDataProviderTests.RemovePerson.cs
@@ -1,4 +1,7 @@
-﻿using CprBroker.PartInterface.Tracking;
+﻿using CprBroker.Engine;
+using CprBroker.PartInterface.Tracking;
+using CprBroker.Providers.CPRDirect;
+using CprBroker.Schemas;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,14 +14,47 @@ namespace CprBroker.Tests.Tracking
     namespace TrackingDataProviderTests
     {
         [TestFixture]
-        public class RemovePerson:PartInterface.TestBase
+        public class RemovePerson : PartInterface.TestBase
         {
+            [SetUp]
+            public void InitBrokerContext()
+            {
+                BrokerContext.Initialize(CprBroker.Utilities.Constants.BaseApplicationToken.ToString(), "");
+            }
+
             [Test]
             public void RemovePerson_EmptyDB_Passes()
             {
                 var prov = new TrackingDataProvider();
                 prov.RemovePerson(new Schemas.PersonIdentifier() { CprNumber = CprBroker.Tests.PartInterface.Utilities.RandomCprNumber(), UUID = Guid.NewGuid() });
             }
+
+            [Test]
+            public void RemovePerson_ExtractExists_Removed(
+                [Values(1, 5, 13, 22, 39, 58)]int pnrIndex)
+            {
+                var pnr = CPRDirect.Utilities.PNRs[pnrIndex];
+
+                Func<string, int> countItems = (s) =>
+                {
+                    using (var dc = new ExtractDataContext())
+                    {
+                        if (string.IsNullOrEmpty(s))
+                            return dc.ExtractItems.Count();
+                        else
+                            return dc.ExtractItems.Where(ei => ei.PNR == pnr).Count();
+                    }
+                };
+                ExtractManager.ImportText(CprBroker.Tests.CPRDirect.Properties.Resources.U12170_P_opgavenr_110901_ADRNVN_FE_FixedLength);
+                Assert.Greater(countItems(pnr), 0);
+                var pId = new PersonIdentifier() { CprNumber = pnr, UUID = Guid.NewGuid() };
+                var prov = new TrackingDataProvider();
+                prov.RemovePerson(pId);
+                Assert.AreEqual(countItems(pnr), 0);
+                Assert.Greater(countItems(null), 0);
+            }
+
+
         }
     }
 }
