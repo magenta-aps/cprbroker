e17860662ecd2a20ea6347e2331ede741d6ac839
beemen <beshara_beemen@hotmail.com>
2016-12-15 10:02:46 +0100

    Person.Delete(..) no longer deletes ActorRef * The deletion causes many deadlocks in the database AND * The table does not contain any personal information

--- PART/Source/CprBroker/PartInterface/Data/Person.cs
+++ PART/Source/CprBroker/PartInterface/Data/Person.cs
@@ -17,8 +17,10 @@ namespace CprBroker.Data.Part
                 var registrationsToDelete = dataContext.PersonRegistrations.Where(pr => pr.UUID == personIdentifier.UUID);
                 dataContext.PersonRegistrations.DeleteAllOnSubmit(registrationsToDelete);
 
-                var actorsToDelete = registrationsToDelete.Select(r => r.ActorRef);
-                dataContext.ActorRefs.DeleteAllOnSubmit(actorsToDelete);
+                // Do not delete ActorRef, it causes deadlocks in the database; and does not contain any personal information
+                /*var actorRefIdsToDelete = registrationsToDelete.Select(r => r.ActorRefId);
+                var actorsToDelete = dataContext.ActorRefs.Where(ar => actorRefIdsToDelete.Contains(ar.ActorRefId));
+                dataContext.ActorRefs.DeleteAllOnSubmit(actorsToDelete);*/
 
                 var personsToDelete = dataContext.Persons.Where(p => p.UUID == personIdentifier.UUID);
                 dataContext.Persons.DeleteAllOnSubmit(personsToDelete);
